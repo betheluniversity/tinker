ad20b489c12d9f1c02b097e5933cf60176ead86d
Eric Jameson <e-jameson@bethel.edu>
2014-06-19 11:43:17 -0500

    move

--- tinker/cascade_events.py
+++ tinker/cascade_events.py
@@ -12,6 +12,36 @@ from tinker import app
 from tinker import cache
 
 
+def dynamic_field(name, values):
+
+    values_list = []
+    for value in values:
+        values_list.append({'value': value})
+    node = {
+        'name': name,
+        'fieldValues': {
+            'fieldValue': values_list,
+        },
+    },
+
+    return node
+
+
+def structured_data_node(id, text, node_type=None):
+
+    if not node_type:
+        node_type = "text"
+
+    node = {
+
+        'identifier': id,
+        'text': text,
+        'type': node_type,
+    }
+
+    return node
+
+
 #just duplicate for now
 def date_to_java_unix(date):
 
--- tinker/web_services.py
+++ tinker/web_services.py
@@ -33,36 +33,6 @@ def delete(page_id):
     return response
 
 
-def dynamic_field(name, values):
-
-    values_list = []
-    for value in values:
-        values_list.append({'value': value})
-    node = {
-        'name': name,
-        'fieldValues': {
-            'fieldValue': values_list,
-        },
-    },
-
-    return node
-
-
-def structured_data_node(id, text, node_type=None):
-
-    if not node_type:
-        node_type = "text"
-
-    node = {
-
-        'identifier': id,
-        'text': text,
-        'type': node_type,
-    }
-
-    return node
-
-
 def publish(page_id):
 
     client = get_client()
