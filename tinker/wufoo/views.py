a8cbfdb8bb87b636f7a1acf289067f94b43d3262
Eric Jameson <e-jameson@bethel.edu>
2014-08-26 09:57:38 -0500

    start working on form embed.

--- tinker/wufoo/views.py
+++ tinker/wufoo/views.py
@@ -6,13 +6,10 @@ import requests
 from flask import request, Blueprint, render_template, jsonify
 
 #Tinker
-from tinker import app
 from tinker import db
 from tinker import cache
 from tinker.wufoo.models import FormInfo
-
-
-import api
+from tinker.wufoo import api
 
 wufoo_blueprint = Blueprint('wufoo', __name__,
                         template_folder='templates', static_folder='static')
@@ -53,10 +50,16 @@ def all_preload():
 
     return "<pre>%s</pre>" % resp
 
+
 @wufoo_blueprint.route("/get-forms") #, methods=['POST'])
 @cache.cached(timeout=500)
 def get_forms():
     return api.get_form_list()
+
+
+@wufoo_blueprint.route('/embed-form/<formhash>')
+def embed_form(formhash):
+    return formhash
 
 
 @wufoo_blueprint.route('/load-form/<formhash>')
