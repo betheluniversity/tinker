32d46811955f52f61498dde8fe2b66a960817b39
Eric Jameson <e-jameson@bethel.edu>
2014-09-23 15:50:24 -0500

    start working on heading upgrade script

--- tinker/heading_upgrade/views.py
+++ tinker/heading_upgrade/views.py
@@ -1,18 +1,42 @@
 __author__ = 'ejc84332'
 
 #python
-import re
-import smtplib
 
-from flask import Blueprint, render_template, abort, request, redirect
-from BeautifulSoup import BeautifulSoup
+from flask import Blueprint
 
-from tinker import app, db, tools
 from tinker.web_services import *
 
 
 heading_upgrade = Blueprint('heading_upgrade', __name__,
                                template_folder='templates')
+
+def upgrade_content(content):
+    """
+    h3->h2
+    h4->h3
+    h5->h4
+    """
+    content = content.replace("<h3>", "<h2>").replace("</h3>", "</h2>")
+    content = content.replace("<h4>", "<h3>").replace("</h4>", "</h3>")
+    content = content.replace("<h5>", "<h4>").replace("</h5>", "</h4>")
+    return content
+
+
+def upgrade_basic_page(page):
+    content = page['asset']['page']['structuredData']['structuredDataNodes']['structuredDataNode'][0]['text']
+    new_content = upgrade_content(content)
+
+    page['asset']['page']['structuredData']['structuredDataNodes']['structuredDataNode'][0]['text'] = new_content
+    asset = {
+        'page': page['asset']['page']
+    }
+    resp = edit(asset)
+    return resp
+
+
+def upgrade_basic_plus_page(page):
+    content = page['asset']['page']['structuredData']['structuredDataNodes']['structuredDataNode']
+    x = 1
 
 
 def inspect_folder(folder_id):
@@ -32,7 +56,14 @@ def inspect_folder(folder_id):
 
 def inspect_page(page_id):
     page = read(page_id)
-    return str(page)
+    page_type = page['asset']['page']['contentTypePath']
+    if page_type == "Basic":
+        upgrade_basic_page(page)
+    elif page_type == "Basic Plus":
+        return str(page)
+        upgrade_basic_plus_page(page)
+
+    return None
 
 @heading_upgrade.route('/')
 def show():
