e47393d777faee95773597b4d426896c270f842f
Eric Jameson <e-jameson@bethel.edu>
2014-11-24 09:03:22 -0600

    don't catch errors so it emails

--- tinker/tools.py
+++ tinker/tools.py
@@ -98,18 +98,13 @@ def clear_image_cache(image_path):
         path = "%s/%s/%s" % (config.THUMBOR_STORAGE_LOCATION.rstrip('/'), digest[:2], digest[2:])
         # remove the file at the path
         # if config.ENVIRON == "prod":
-        try:
-            call(['rm', path])
-        except:
-            continue
+        call(['rm', path])
+
     # now the result storage
     file_name = image_path.split('/')[-1]
     call_cmd = config.THUMBOR_CALL_CMD
     call_cmd[3] = file_name
 
-    try:
-        call(call_cmd)
-    except:
-        pass
+    call(call_cmd)
 
     return "done"
