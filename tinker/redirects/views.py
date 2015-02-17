859a33abbc178016791667fe477c0696f0a081ba
Caleb Schwarze <ces55739@dhcp-140-88-95-77.itstm-websrv-29.vlan.net.its.bethel.edu>
2015-02-17 15:41:27 -0600

    added /public to api-submit cals

--- tinker/redirects/views.py
+++ tinker/redirects/views.py
@@ -81,8 +81,7 @@ def new_redirect_submti():
 
     return str(redirect)
 
-
-@redirect_blueprint.route('/api-submit', methods=['get', 'post'])
+@redirect_blueprint.route('/public/api-submit', methods=['get', 'post'])
 def new_api_submit():
     body = request.form['body']
 
@@ -116,35 +115,35 @@ def new_api_submit():
     return str(redirect)
 
 
-@redirect_blueprint.route('/api-submit-asset-expiration', methods=['get', 'post'])
+@redirect_blueprint.route('/public/api-submit-asset-expiration', methods=['get', 'post'])
 def new_api_submit_asset_expiration():
     smtp_obj = smtplib.SMTP('localhost')
     smtp_obj.sendmail("tinker@bethel.edu", ['ces55739@bethel.edu'], "TEST: Began api-submit-asset-expiration")
 
-    # subject = request.form['subject']
-    # soup = BeautifulSoup(subject)
-    # all_text = ''.join(soup.findAll(text=True))
-    # try:
-    #     from_path = "/" + all_text.replace("Asset expiration notice for Public:", "")
-    #     to_url = "https://www.bethel.edu/employment/openings/postings/job-closed"
-    #     redirect = BethelRedirect(from_path=from_path, to_url=to_url)
-    #     db.session.add(redirect)
-    #     db.session.commit()
-    # except:
-    #     message = "redirect from %s to %s already exists" % (from_path, to_url)
-    #     sender = 'tinker@bethel.edu'
-    #     receivers = ['ces55739@bethel.edu']
-    #
-    #     smtp_obj = smtplib.SMTP('localhost')
-    #     smtp_obj.sendmail(sender, receivers, message)
-    #     print "Successfully sent email"
-    #     db.session.rollback()
-    #     return "sent email notice"
-    #
-    # if redirect:
-    #     create_redirect_text_file()
-    #
-    # return str(redirect)
+    subject = request.form['subject']
+    soup = BeautifulSoup(subject)
+    all_text = ''.join(soup.findAll(text=True))
+    try:
+        from_path = "/" + all_text.replace("Asset expiration notice for Public:", "")
+        to_url = "https://www.bethel.edu/employment/openings/postings/job-closed"
+        redirect = BethelRedirect(from_path=from_path, to_url=to_url)
+        db.session.add(redirect)
+        db.session.commit()
+    except:
+        message = "redirect from %s to %s already exists" % (from_path, to_url)
+        sender = 'tinker@bethel.edu'
+        receivers = ['ces55739@bethel.edu']
+
+        smtp_obj = smtplib.SMTP('localhost')
+        smtp_obj.sendmail(sender, receivers, message)
+        print "Successfully sent email"
+        db.session.rollback()
+        return "sent email notice"
+
+    if redirect:
+        create_redirect_text_file()
+
+    return str(redirect)
     return "Test"
 
 
