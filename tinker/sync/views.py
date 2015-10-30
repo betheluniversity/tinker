19b4b76a29357ed0737c7891bbb3b0ec7b8e8ab9
c-schwarze <ces55739@bethel.edu>
2015-10-30 15:35:15 -0500

    updated sync

--- tinker/sync/views.py
+++ tinker/sync/views.py
@@ -18,9 +18,9 @@ def home():
 @sync_blueprint.route('/all')
 def show():
     # don't pull locally. It's just a bad idea.
-    if 'User' not in app.config['INSTALL_LOCATION']:
-        import commands
-        commands.getoutput("cd " + app.config['INSTALL_LOCATION'] + "; git pull")
+    # if 'User' not in app.config['INSTALL_LOCATION']:
+    import commands
+    commands.getoutput("cd " + app.config['INSTALL_LOCATION'] + "; rm tinker/sync/metadata.py; git pull")
 
     sync_metadataset(app.config['METADATA_EVENT_ID'])
     sync_metadataset(app.config['METADATA_ROBUST_ID'])
