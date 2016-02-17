d84286b8d434af3488cba10d18cf88c48bc67f7a
Nathan Li <nal64753@bethel.edu>
2016-02-17 14:20:51 -0600

    add base table and make page

---
+++ tinker/roles/views.py
@@ -0,0 +1,13 @@
+__author__ = 'ces55739'
+
+# tinker
+
+
+from flask import Blueprint, render_template, abort, request
+
+blink_roles_blueprint = Blueprint('blink_roles_blueprint', __name__, template_folder='templates')
+
+@blink_roles_blueprint.route('/')
+def home():
+    return render_template('blink-roles-home.html', **locals())
+
