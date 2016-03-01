d2a263269928c81b2d712d66cdaf2b689a0c72ac
Nathan Li <nal64753@bethel.edu>
2016-03-01 13:52:42 -0600

    move sync and role subfolders to admin

---
+++ tinker/admin/roles/views.py
@@ -0,0 +1,12 @@
+__author__ = 'ces55739'
+from roledata import uid, portal
+# tinker
+from flask import Blueprint, render_template
+
+blink_roles_blueprint = Blueprint('blink_roles_blueprint', __name__, template_folder='templates')
+
+@blink_roles_blueprint.route('/')
+def home():
+    uid_list = uid
+    portal_list = portal
+    return render_template('blink-roles-home.html', **locals())
---
+++ tinker/admin/sync/metadata.py
@@ -0,0 +1,120 @@
+school = [
+    'Bethel University',
+    'College of Arts & Sciences',
+    'College of Adult & Professional Studies',
+    'Graduate School',
+    'Bethel Seminary',
+]
+
+undergrad_programs = [
+    "None",
+    "Anthropology, Sociology, & Reconciliation",
+    "Art & Design",
+    "Athletic Training",
+    "Biblical & Theological Studies",
+    "Biokinetics",
+    "Biological Sciences",
+    "Business & Economics",
+    "Chemistry",
+    "Communication Studies",
+    "Education",
+    "English",
+    "Environmental Studies",
+    "General Education",
+    "Health and Physical Education",
+    "History",
+    "Honors",
+    "Math & Computer Science",
+    "Modern World Languages",
+    "Music",
+    "Nursing",
+    "Philosophy",
+    "Physics & Engineering",
+    "Political Science",
+    "Preprofessional",
+    "Psychology",
+    "Social Work",
+    "Theatre Arts",
+]
+
+adult_undergrad_programs = [
+    "None",
+    "Accounting (B.S.)",
+    "Addiction Studies Certificate",
+    "Alcohol and Drug Counseling License",
+    "Associate's Degree",
+    "Business Management (B.A.)",
+    "Christian Ministries (B.A.)",
+    "Communication Studies (B.A.)",
+    "Finance (B.S.)",
+    "Human Resource Management Certificate",
+    "Human Services (B.A.)",
+    "Nursing (RN to B.S.)",
+    "Organizational Leadership (B.A.)",
+]
+
+seminary_programs = [
+    "None",
+    "Anglican Studies Certificate",
+    "Children's & Family Ministry (M.A.)",
+    "Christian Studies Certificate",
+    "Christian Thought (M.A.)",
+    "Doctor of Ministry",
+    "Marital & Family Therapy (M.A.)",
+    "Marriage & Family Therapy Certificate",
+    "Marriage & Family Therapy (M.A.)",
+    "Master of Divinity",
+    "Mental Health Counseling (M.A.)",
+    "Ministry Practice Certificate",
+    "Ministry Practice (M.A.)",
+    "Theological Studies Certificate",
+    "Theological Studies (M.A.)",
+    "Transformational Leadership (M.A.)",
+    "Young Life Youth Ministry Certificate",
+]
+
+graduate_programs = [
+    "None",
+    "Certificate in International Baccalaureate Teaching and Learning",
+    "Child and Adolescent Mental Health Certificate",
+    "Counseling Psychology (M.A.)",
+    "Director of Special Education License",
+    "Ed.D.",
+    "Education K-12 (M.A.)",
+    "Emerging Leaders in Private Colleges Certificate",
+    "ENVoY Classroom Management Strategies Certificate",
+    "Gerontology Certificate",
+    "Gerontology (M.A.)",
+    "K-12 Principal License",
+    "Leadership Foundations Certificate",
+    "MBA",
+    "Nurse Educator Certificate",
+    "Nurse-Midwifery (M.S.)",
+    "Nursing (M.S.)",
+    "Physician Assistant (M.S.)",
+    "Special Education (M.A.)",
+    "STEM in K-12 Education Certificate",
+    "Strategic Leadership (M.A.)",
+    "Superintendent License",
+    "Teaching (M.A.)",
+    "Teacher Coordinator of Work-based Learning License",
+    "Teachers of Computer, Keyboarding, and Related Technology Applications License",
+    "Teaching License",
+]
+
+locations = [
+    'Arden Hills',
+    'Bloomington',
+    'North Hennepin Community College',
+    'Online',
+    'San Diego',
+    'St. Paul',
+]
+
+data_to_add = {}
+data_to_add['school'] = school
+data_to_add['department'] = undergrad_programs
+data_to_add['adult-undergrad-program'] = adult_undergrad_programs
+data_to_add['graduate-program'] = graduate_programs
+data_to_add['seminary-program'] = seminary_programs
+data_to_add['location'] = locations
