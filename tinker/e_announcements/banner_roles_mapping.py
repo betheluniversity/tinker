af3f11c3e642f3721dd832ce7edf91c4a0406326
c-schwarze <ces55739@bethel.edu>
2016-03-11 16:08:45 -0600

    Updated form

---
+++ tinker/e_announcements/banner_roles_mapping.py
@@ -0,0 +1,19 @@
+def get_banner_roles_mapping():
+    return [
+        'CAS',
+        'CAPS',
+        'GS',
+        'BSSP-TRADITIONAL',
+        'BSSP-DISTANCE',
+        'BSSD-TRADITIONAL',
+        'BSSD-DISTANCE',
+        'BSOE-TRADITIONAL',
+        'BSOE-DISTANCE',
+        'CAS',
+        'CAPS',
+        'GS',
+        'BSSP',
+        'BSSD',
+        'St. Paul',
+        'San Diego'
+    ]
