6164b75f0f4ee26cb7e5cf1876e8453e01159c6a
josiahtillman <jot43536@bethel.edu>
2016-06-01 16:16:42 -0500

    Added cache files and 2 more requirements

--- tinker/admin/cache/views.py
+++ tinker/admin/cache/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, abort, session
+from flask import Blueprint, render_template, abort, session, request
+from tinker import tools
 
 cache_blueprint = Blueprint('cache_blueprint', __name__, template_folder='templates')
 
@@ -10,3 +11,13 @@ def before_request():
 @cache_blueprint.route('/')
 def home():
     return render_template('cache-home.html', **locals())
+
+@cache_blueprint.route('/submit', methods=['post'])
+def submit():
+    path = request.form['url']
+    return cache_clear(path)
+
+def cache_clear(img_path=None):
+    if not img_path:
+        return "Please enter in a path."
+    return tools.clear_image_cache(img_path)
