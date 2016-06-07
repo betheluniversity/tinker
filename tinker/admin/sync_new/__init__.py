14fbc6e03244278211f8148a47e313c51e48d423
c-schwarze <ces55739@bethel.edu>
2016-06-07 16:13:36 -0500

    Updated to work with bu-cascade v2.07

--- tinker/admin/sync_new/__init__.py
+++ tinker/admin/sync_new/__init__.py
@@ -31,23 +31,22 @@ class SyncView(FlaskView):
 
         metadata_sets = [
             # app.config['METADATA_EVENT_ID'],
-            'f28775138c5865134b131aec94dc5286'
             # app.config['METADATA_ROBUST_ID'],
             # app.config['METADATA_JOB_POSTING_ID'],
             # app.config['METADATA_PORTAL_ROLES_ID']
         ]
         self.base.sync_metadata_sets(metadata_sets, data_to_add)
 
-        # data_definitions = [
-        #     app.config['DATA_DEF_FACULTY_BIO_ID'],
-        #     app.config['DATA_DEF_PROGRAM_FEED_ID'],
-        #     app.config['DATA_DEF_PROGRAM_BLOCK_ID'],
-        #     app.config['DATA_DEF_PORTAL_CHANNEL_ID'],
-        #     app.config['DATA_DEF_PORTAL_CHANNEL_ID'],
-        #     app.config['DATA_DEF_PORTAL_TAB_ID'],
-        #     app.config['DATA_DEF_PROGRAM_SEARCH_ID']
-        # ]
-        # self.base.sync_data_definitions(data_definitions, data_to_add)
+        data_definitions = [
+            # app.config['DATA_DEF_FACULTY_BIO_ID'],
+            # app.config['DATA_DEF_PROGRAM_FEED_ID'],
+            # app.config['DATA_DEF_PROGRAM_BLOCK_ID'],
+            # app.config['DATA_DEF_PORTAL_CHANNEL_ID'],
+            # app.config['DATA_DEF_PORTAL_CHANNEL_ID'],
+            # app.config['DATA_DEF_PORTAL_TAB_ID'],
+            # app.config['DATA_DEF_PROGRAM_SEARCH_ID']
+        ]
+        self.base.sync_data_definitions(data_definitions, data_to_add)
 
         return render_template('sync.html', **locals())
 
--- tinker/admin/sync_new/sync_controller.py
+++ tinker/admin/sync_new/sync_controller.py
@@ -12,8 +12,17 @@ class SyncController(TinkerController):
             try:
                 asset = self.read_metadata_set(metadata_set)
                 metadata_asset, empty_variable, empty_variable = asset.get_asset()
-                print metadata_asset
-                # asset_tools.update(metadata_asset, 'seminary-program', data_to_add['seminary-program'])
+
+                asset_tools.update_metadata_set(metadata_asset, 'roles', data_to_add['roles'])
+                asset_tools.update_metadata_set(metadata_asset, 'school', data_to_add['school'])
+                asset_tools.update_metadata_set(metadata_asset, 'department', data_to_add['department'])
+                asset_tools.update_metadata_set(metadata_asset, 'cas-departments', data_to_add['department'])
+                asset_tools.update_metadata_set(metadata_asset, 'adult-undergrad-program', data_to_add['adult-undergrad-program'])
+                asset_tools.update_metadata_set(metadata_asset, 'graduate-program', data_to_add['graduate-program'])
+                asset_tools.update_metadata_set(metadata_asset, 'seminary-program', data_to_add['seminary-program'])
+                asset_tools.update_metadata_set(metadata_asset, 'degree', data_to_add['degree'])
+
+                asset.edit_asset(metadata_asset)
 
             except:
                 # todo: log an error here
@@ -22,5 +31,24 @@ class SyncController(TinkerController):
     # Todo create this
     def sync_data_definitions(self, data_definitions, data_to_add):
         for data_definition in data_definitions:
-            data_definition_asset = self.read_datadefinition(data_definition)
-            print data_definition_asset.get_asset()
+            try:
+                asset = self.read_datadefinition(data_definition)
+                data_definition_asset, empty_variable, empty_variable = asset.get_asset()
+
+                # asset_tools.update_data_definition(data_definition_asset, 'roles', data_to_add['roles'])
+
+                # todo: make sure to test with duplicates, not the real data definitions
+                # maybe add an additional optional parameter to the update.
+                # if "job-titles" in el.attrib[
+                # elif "program_filters" in el.attrib['identifier']:  # for Program Feeds | location
+                # elif "concentration" in el.attrib['identifier']:  # for Program Blocks | location, cohort delivery
+                # elif 'roles' in el.attrib['identifier']:  # for Portal - Tab | roles
+                # elif 'sections' in el.attrib['identifier']:  # for Portal - Channel Block | roles
+                # elif 'program-search-sync-data' in el.attrib['identifier']:  # for Program Search | school, cohort delivery, degree type
+
+
+                asset.edit_asset(data_definition_asset)
+
+            except:
+                # todo: log an error here
+                pass
