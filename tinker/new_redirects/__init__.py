de1b022b07b3f0e968a7db0b009aa97bdb26fa3a
c-schwarze <ces55739@bethel.edu>
2016-06-07 15:25:35 -0500

    A few extra redirects

--- tinker/new_redirects/__init__.py
+++ tinker/new_redirects/__init__.py
@@ -1,6 +1,7 @@
 __author__ = 'ejc84332'
 
 import re
+import smtplib
 
 from datetime import datetime
 
@@ -46,6 +47,21 @@ class RedirectsView(FlaskView):
             return "fail"
         return "deleted %s" % resp
 
+    # Finds all redirects associated with the from path entered
+    @route("/search", methods=['post'])
+    def search(self):
+        # todo: limit results to...100?
+        search_type = request.form['type']
+        search_query = request.form['search'] + "%"
+        if self.search == "%" or search_type not in ['from_path', 'to_url']:
+            return ""
+        if search_type == 'from_path':
+            redirects = BethelRedirect.query.filter(BethelRedirect.from_path.like(search_query)).limit(100).all()
+        else:
+            redirects = BethelRedirect.query.filter(BethelRedirect.to_url.like(search_query)).limit(100).all()
+        redirects.sort()
+        return render_template('redirect-ajax.html', **locals())
+
     # Saves the new redirect created
     @route("/new-redirect-submit", methods=['post'])
     def new_redirect_submit(self):
@@ -80,69 +96,10 @@ class RedirectsView(FlaskView):
 
         return str(redirect)
 
-    # TODO Comment on what this does
-    @route('/new-internal-submit/<from_path>/<to_url>', methods=['get', 'post'])
-    def new_internal_redirect_submit(self, from_path, to_url):
-        # added logic to have Tinker be able to internally create a redirect
-
-        if not from_path.startswith("/"):
-            from_path = "/%s" % from_path
-
-        # if one from the current from exists, remove it.
-        try:
-            redirect = BethelRedirect.query.get(from_path)
-            self.add.database_delete(redirect)
-
-            # following variable isn't being used... USED ANYWHERE ELSE?
-            # resp = self.base.create_redirect_text_file()
-            app.logger.debug(": Correctly deleted if necessary")
-        except:
-            print "no deletion was made"
-
-        # create the redirect
-        try:
-            redirect = BethelRedirect(from_path=from_path, to_url=to_url)
-            self.add.database_add(redirect)
-            print "Successfully created a internal redirect"
-            app.logger.debug(": Correctly created a new one")
-        except:
-            db.session.rollback()
-
-        # Update the file after every submit?
-        self.base.create_redirect_text_file()
-
-        app.logger.debug(": Correctly finished")
-        return str(redirect)
-
-    # TODO Comment on what this does
-    @route('/public/api-submit-asset-expiration', methods=['get', 'post'])
-    def new_api_submit_asset_expiration(self):
-        subject = request.form['subject']
-        soup = BeautifulSoup(subject)
-        all_text = ''.join(soup.findAll(text=True))
-
-        try:
-            lines = all_text.split("Asset expiration notice for Public:")
-            from_path = "/" + lines[1].lstrip().rstrip()
-            to_url = "https://www.bethel.edu/employment/openings/postings/job-closed"
-            redirect = BethelRedirect(from_path=from_path, to_url=to_url)
-            self.add.database_add(redirect)
-
-        except:
-            message = "redirect from %s to %s already exists" % (from_path, to_url)
-            sender = 'tinker@bethel.edu'
-            receivers = ['ces55739@bethel.edu']
-
-            smtp_obj = self.basesmtplib.SMTP('localhost')
-            smtp_obj.sendmail(sender, receivers, message)
-            print "Successfully sent email"
-            db.session.rollback()
-            return "sent email notice"
-
-        if redirect:
-            self.base.create_redirect_text_file()
-
-        return str(redirect)
+    # Updates the redirect text file upon request
+    def compile(self):
+        resp = self.base.create_redirect_text_file()
+        return resp
 
     # Deletes expired redirects on the day of its expiration date
     def expire(self):
@@ -154,46 +111,7 @@ class RedirectsView(FlaskView):
         self.base.create_redirect_text_file()
         return 'done'
 
-    # Test todo needed?
-    def test(self):
-        redirects = BethelRedirect.query.all()
-        resp = ["<pre>"]
-        for redirect in redirects:
-
-            from_path = redirect.from_path
-            to_url = redirect.to_url
-
-            if 'bethel.edu' not in to_url:
-                continue
-            try:
-                to_path = to_url.split('.edu')[1]
-            except:
-                x = 2
-            if from_path == to_path:
-                self.add.database_delete(redirect)
-
-                resp.append("deleted %s : %s" % (from_path, to_path))
-
-        self.base.create_redirect_text_file()
-        resp.append("</pre>")
-        return '\n'.join(resp)
-
-    # Finds all redirects associated with the from path entered
-    @route("/search", methods=['post'])
-    def search(self):
-        # todo: limit results to...100?
-        search_type = request.form['type']
-        search_query = request.form['search'] + "%"
-        if self.search == "%" or search_type not in ['from_path', 'to_url']:
-            return ""
-        if search_type == 'from_path':
-            redirects = BethelRedirect.query.filter(BethelRedirect.from_path.like(search_query)).limit(100).all()
-        else:
-            redirects = BethelRedirect.query.filter(BethelRedirect.to_url.like(search_query)).limit(100).all()
-        redirects.sort()
-        return render_template('redirect-ajax.html', **locals())
-
-    # TODO Comment on what this does
+    # This creates redirects generically from a google script and the webmaster email box
     @route('/public/api-submit', methods=['get', 'post'])
     def new_api_submit(self):
         body = request.form['body']
@@ -210,26 +128,44 @@ class RedirectsView(FlaskView):
                     from_url, to_url = line.split()
                     from_path = from_url.replace("www.bethel.edu", "").replace("http://", "").replace('https://', "")
                     redirect = BethelRedirect(from_path=from_path, to_url=to_url)
-                    self.add.database_add(redirect)
-
+                    self.base.database_add(redirect)
             except:
-                # message = "redirect from %s to %s already exists" % (from_url, to_url)
-                # sender = 'tinker@bethel.edu'
-                # receivers = ['e-jameson@bethel.edu', 'a-vennerstrom@bethel.edu', 'ces55739@bethel.edu']
-                #
-                # smtp_obj = smtplib.SMTP('localhost')
-                # smtp_obj.sendmail(sender, receivers, message)
-                # print "Successfully sent email"
                 db.session.rollback()
-                # return "sent email notice"
 
         if redirect:
             self.base.create_redirect_text_file()
         return str(redirect)
 
-    # Updates the redirect text file upon request
-    def compile(self):
-        resp = self.base.create_redirect_text_file()
-        return resp
+    # This creates a redirect for job postings from a google script and the webmaster email box
+    @route('/public/api-submit-asset-expiration', methods=['get', 'post'])
+    def new_api_submit_asset_expiration(self):
+        from_path = ''
+        to_url = ''
+        subject = request.form['subject']
+        soup = BeautifulSoup(subject)
+        all_text = ''.join(soup.findAll(text=True))
+
+        try:
+            lines = all_text.split("Asset expiration notice for Public:")
+            from_path = "/" + lines[1].lstrip().rstrip()
+            to_url = "https://www.bethel.edu/employment/openings/postings/job-closed"
+            redirect = BethelRedirect(from_path=from_path, to_url=to_url)
+            self.base.database_add(redirect)
+
+        except:
+            message = "redirect from %s to %s already exists" % (from_path, to_url)
+            sender = 'tinker@bethel.edu'
+            receivers = ['ces55739@bethel.edu']
+
+            smtp_obj = smtplib.SMTP('localhost')
+            smtp_obj.sendmail(sender, receivers, message)
+            print "Successfully sent email"
+            db.session.rollback()
+            return "sent email notice"
+
+        if redirect:
+            self.base.create_redirect_text_file()
+
+        return str(redirect)
 
 RedirectsView.register(RedirectsBlueprint)
