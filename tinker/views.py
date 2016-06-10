3966ed4c796398ad8a75c6ac67aeb3796860c3c0
c-schwarze <ces55739@bethel.edu>
2016-06-10 16:11:48 -0500

    added get-image back

--- tinker/views.py
+++ tinker/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from tinker import app
-
+from flask import send_file
 
 @app.route('/')
 def home():
@@ -12,6 +12,9 @@ def home():
 def about():
     return render_template('about-page.html', **locals())
 
+@app.route('/get-image/<image_name>')
+def get_image(image_name):
+    return send_file('images/' + image_name, mimetype='image/png')
 
 @app.route('/read/<read_id>')
 def read_route(read_id):
