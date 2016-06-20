5844fa825b4578112ac94dd9025abe29bae028cf
Tim Heck <tih99924@bethel.edu>
2016-06-20 11:57:39 -0500

    Added cascade_events
    Added needed cascade_events methods to the controller

--- tinker/events/Events_Controller.py
+++ tinker/events/Events_Controller.py
@@ -1,6 +1,19 @@
 from tinker.tinker_controller import TinkerController
 import json
+# involves wysiwyg (need to get rid of)
 from tinker.events.cascade_events import *
+
+import re
+import urllib2
+from arrow import arrow
+
+from xml.etree import ElementTree as ET
+from operator import itemgetter
+
+# local
+from tinker import app
+
+from flask import render_template, session
 
 
 class EventsController(TinkerController):
@@ -75,6 +88,25 @@ class EventsController(TinkerController):
     #             items = [item.value for item in field.fieldValues.fieldValue]
     #             edit_data[field.name.replace('-', '_')] = items
 
+    # web services methods
+    def date_to_java_unix(self, date):
+
+        return int(self.base.datetime.datetime.strptime(date, '%B %d  %Y, %I:%M %p').strftime("%s")) * 1000
+
+    def friendly_date_range(self, start, end):
+        date_format = "%B %d, %Y %I:%M %p"
+
+        start_check = arrow.get(start)
+        end_check = arrow.get(end)
+
+        if start_check.year == end_check.year and start_check.month == end_check.month and start_check.day == end_check.day:
+            return "%s - %s" % (self.base.datetime.datetime.fromtimestamp(int(start)).strftime(date_format),
+                                self.base.datetime.datetime.fromtimestamp(int(end)).strftime("%I:%M %p"))
+        else:
+            return "%s - %s" % (self.base.datetime.datetime.fromtimestamp(int(start)).strftime(date_format),
+                                self.base.datetime.datetime.fromtimestamp(int(end)).strftime(date_format))
+
+    # cascade event methods
     def get_forms_for_user(self, username):
         # todo: move this to config
         if app.config['ENVIRON'] != "prod":
@@ -82,7 +114,7 @@ class EventsController(TinkerController):
             form_xml = ET.fromstring(response.read())
         else:
             form_xml = ET.parse('/var/www/staging/public/_shared-content/xml/events.xml').getroot()
-        matches = traverse_event_folder(form_xml, username)
+        matches = self.traverse_event_folder(form_xml, username)
         matches = sorted(matches, key=itemgetter('title'), reverse=False)
 
         return matches
@@ -129,7 +161,7 @@ class EventsController(TinkerController):
                     # end = int(date.find('end-date').text) / 1000
                     start = int(date.find('start-date').text) / 1000
                     end = int(date.find('end-date').text) / 1000
-                    dates_str.append(friendly_date_range(start, end))
+                    dates_str.append(self.friendly_date_range(start, end))
 
                 page_values = {
                     'author': author,
@@ -151,7 +183,7 @@ class EventsController(TinkerController):
             title = "-- %s" % title
         workflow = {
             "workflowName": "%s, %s at %s (%s)" %
-                            (title, time.strftime("%m-%d-%Y"), time.strftime("%I:%M %p"), username),
+                            (title, self.base.time.strftime("%m-%d-%Y"), self.base.time.strftime("%I:%M %p"), username),
             "workflowDefinitionId": "1ca9794e8c586513742d45fd39c5ffe3",
             "workflowComments": "New event submission"
         }
@@ -208,17 +240,17 @@ class EventsController(TinkerController):
             # Convert to a unix timestamp, and then multiply by 1000 because Cascade uses Java dates
             # which use milliseconds instead of seconds
             try:
-                start = date_to_java_unix(start)
+                start = self.date_to_java_unix(start)
             except ValueError as e:
-                app.logger.error(time.strftime("%c") + ": error converting start date " + str(e))
+                app.logger.error(self.base.time.strftime("%c") + ": error converting start date " + str(e))
                 start = None
             try:
-                end = date_to_java_unix(end)
+                end = self.date_to_java_unix(end)
             except ValueError as e:
-                app.logger.error(time.strftime("%c") + ": error converting end date " + str(e))
+                app.logger.error(self.base.time.strftime("%c") + ": error converting end date " + str(e))
                 end = None
 
-            dates.append(event_date(start, end, all_day))
+            dates.append(self.event_date(start, end, all_day))
 
         return dates
 
@@ -240,21 +272,21 @@ class EventsController(TinkerController):
 
         # Create a list of all the data nodes
         structured_data = [
-            structured_data_node("main-content", escape_wysiwyg_content(add_data['main_content'])),
-            structured_data_node("questions", escape_wysiwyg_content(add_data['questions'])),
-            structured_data_node("link", escape_wysiwyg_content(add_data['link'])),
-            structured_data_node("cancellations", add_data['cancellations']),
-            structured_data_node("registration-details", escape_wysiwyg_content(add_data['registration_details'])),
-            structured_data_node("registration-heading", add_data['registration_heading']),
-            structured_data_node("cost", add_data['cost']),
-            structured_data_node("sponsors", escape_wysiwyg_content(add_data['sponsors'])),
-            structured_data_node("maps-directions", escape_wysiwyg_content(add_data['maps_directions'])),
-            structured_data_node("off-campus-location", add_data['off_campus_location']),
-            structured_data_node("on-campus-location", add_data['on_campus_location']),
-            structured_data_node("other-on-campus", add_data['other_on_campus']),
-            structured_data_node("location", add_data['location']),
-            structured_data_node("featuring", add_data['featuring']),
-            structured_data_node("wufoo-code", add_data['wufoo_code']),
+            self.structured_data_node("main-content", escape_wysiwyg_content(add_data['main_content'])),
+            self.structured_data_node("questions", escape_wysiwyg_content(add_data['questions'])),
+            self.structured_data_node("link", escape_wysiwyg_content(add_data['link'])),
+            self.structured_data_node("cancellations", add_data['cancellations']),
+            self.structured_data_node("registration-details", escape_wysiwyg_content(add_data['registration_details'])),
+            self.structured_data_node("registration-heading", add_data['registration_heading']),
+            self.structured_data_node("cost", add_data['cost']),
+            self.structured_data_node("sponsors", escape_wysiwyg_content(add_data['sponsors'])),
+            self.structured_data_node("maps-directions", escape_wysiwyg_content(add_data['maps_directions'])),
+            self.structured_data_node("off-campus-location", add_data['off_campus_location']),
+            self.structured_data_node("on-campus-location", add_data['on_campus_location']),
+            self.structured_data_node("other-on-campus", add_data['other_on_campus']),
+            self.structured_data_node("location", add_data['location']),
+            self.structured_data_node("featuring", add_data['featuring']),
+            self.structured_data_node("wufoo-code", add_data['wufoo_code']),
             image_node,
         ]
         # Add the dates at the end of the data
@@ -268,26 +300,185 @@ class EventsController(TinkerController):
         }
 
         # put it all into the final asset with the rest of the SOAP structure
-        hide_site_nav, parent_folder_path = get_event_folder_path(add_data)
+        hide_site_nav, parent_folder_path = self.get_event_folder_path(add_data)
 
         # create the dynamic metadata dict
         dynamic_fields = {
             'dynamicField': [
-                dynamic_field('general', add_data['general']),
-                dynamic_field('offices', add_data['offices']),
-                dynamic_field('cas-departments', add_data['cas_departments']),
-                dynamic_field('graduate-program', add_data['graduate_program']),
-                dynamic_field('adult-undergrad-program', add_data['adult_undergrad_program']),
-                dynamic_field('seminary-program', add_data['seminary_program']),
-                dynamic_field('internal', add_data['internal']),
-                dynamic_field('hide-site-nav', [hide_site_nav]),
-                dynamic_field('tinker-edits', '1')
+                self.dynamic_field('general', add_data['general']),
+                self.dynamic_field('offices', add_data['offices']),
+                self.dynamic_field('cas-departments', add_data['cas_departments']),
+                self.dynamic_field('graduate-program', add_data['graduate_program']),
+                self.dynamic_field('adult-undergrad-program', add_data['adult_undergrad_program']),
+                self.dynamic_field('seminary-program', add_data['seminary_program']),
+                self.dynamic_field('internal', add_data['internal']),
+                self.dynamic_field('hide-site-nav', [hide_site_nav]),
+                self.dynamic_field('tinker-edits', '1')
             ],
         }
 
+    def dynamic_field(self, name, values):
+
+        values_list = []
+        for value in values:
+            values_list.append({'value': value})
+        node = {
+            'name': name,
+            'fieldValues': {
+                'fieldValue': values_list,
+            },
+        }
+
+        return node
+
+    def read_events_base_asset(self):
+
+        base_asset = self.base.read("/_cascade/base-assets/folders/event-folder", "folder")
+        if 'success = "false"' in str(base_asset):
+            return None
+
+        asset = base_asset.asset.folder.metadata.dynamicFields.dynamicField
+
+        # if there is no metadata, exit
+        if len(asset) == 0:
+            return None
+
+        dynamic_metadata = []
+        for field in asset:
+            # Builds the value string of 1 value
+            field_value = None
+            if field.fieldValues[0][0].value is not None:
+                field_value = str(field.fieldValues[0][0].value)
+
+            # create the dynamic metadata with values
+            dynamic_metadata.append(
+                {
+                    'name': str(field.name),
+                    'fieldValues': {
+                        'fieldValue':
+                        [{
+                            'value': field_value
+                        }]
+                    }
+                })
+
+        return dynamic_metadata
+
+    # Returns (content/config path, parent path)
+    def get_event_folder_path(self, data):
+        # Check to see if this event should go in a specific folder
+
+        def common_elements(list1, list2):
+            # helper function to see if two lists share items
+            return [element for element in list1 if element in list2]
+
+        # Find the year we want
+        max_year = self.get_year_folder_value(data)
+
+        path = "events/%s" % max_year
+        hide_site_nav = "Hide"
+
+        if 'general' in data:
+            general = data['general']
+        else:
+            general = []
+
+        if 'offices' in data:
+            offices = data['offices']
+        else:
+            offices = []
+
+        if 'Athletics' in general:
+            hide_site_nav = "Hide"
+            path = "events/%s/athletics" % max_year
+
+        elif common_elements(['Johnson Gallery', 'Olson Gallery', 'Art Galleries'],  general):
+            hide_site_nav = "Do not hide"
+            path = "events/arts/galleries/exhibits/%s" % max_year
+
+        elif 'Music Concerts' in general:
+            hide_site_nav = "Do not hide"
+            path = 'events/arts/music/%s' % max_year
+
+        elif 'Theatre' in general:
+            hide_site_nav = "Do not hide"
+            path = 'events/arts/theatre/%s' % max_year
+
+        elif any("Chapel" in s for s in general):
+            hide_site_nav = "Hide"
+            path = 'events/%s/chapel' % max_year
+
+        elif 'Library' in general:
+            hide_site_nav = "Hide"
+            path = "events/%s/library" % max_year
+
+        elif 'Bethel Student Government' in offices:
+            hide_site_nav = "Hide"
+            path = "events/%s/bsg" % max_year
+
+        elif any("Admissions" in s for s in offices):
+            hide_site_nav = "Hide"
+            path = 'events/%s/admissions' % max_year
+
+        self.create_event_folder(path)
+
+        return hide_site_nav, path
+
+    def create_event_folder(self, folder_path):
+        if folder_path[0] == "/":
+            folder_path = folder_path[1:]  # removes the extra "/"
+
+        old_folder_asset = self.base.read("/" + folder_path, "folder")
+
+        if 'success = "false"' in str(old_folder_asset):
+
+            array = folder_path.rsplit("/", 1)
+            parent_path = array[0]
+            name = array[1]
+
+            asset = {
+                'folder': {
+                    'metadata': {
+                        'title': name,
+                        'dynamicFields': {
+                            'dynamicField': self.read_events_base_asset()
+                        }
+                    },
+                    'metadataSetPath': "Basic",
+                    'name': name,
+                    'parentFolderPath': parent_path,
+                    'siteName': "Public"
+                }
+            }
+
+            auth = app.config['CASCADE_LOGIN']
+            client = self.base.get_client()
+
+            username = session['username']
+
+            response = client.service.create(auth, asset)
+            # todo is the next line needed?
+            response = app.logger.debug(self.base.time.strftime("%c") + ": New folder creation by " + username + " " + str(response))
+            return True
+        return False
+
+    def structured_data_node(self, node_id, text, node_type=None):
+
+        if not node_type:
+            node_type = "text"
+
+        node = {
+
+            'identifier': node_id,
+            'text': text,
+            'type': node_type,
+        }
+
+        return node
+
     def get_current_year_folder(self, event_id):
         # read in te page and find the current year
-        asset = read(event_id)
+        asset = self.base.read(event_id)
         path = asset.asset.page.path
         try:
             year = re.search('events/(\d{4})/', path).group(1)
@@ -300,14 +491,123 @@ class EventsController(TinkerController):
 
         max_year = 0
         for node in dates:
-            date_data = read_date_data_dict(node[0])
-            end_date = string_to_datetime(date_data['end-date'])
+            date_data = self.read_date_data_dict(node[0])
+            end_date = self.string_to_datetime(date_data['end-date'])
             try:
                 year = end_date.year
             except AttributeError:
                 # if end_date is none and this fails, revert to current year.
-                year = datetime.date.today().year
+                year = self.base.datetime.date.today().year
             if year > max_year:
                 max_year = year
 
         return max_year
+
+    # just duplicate a bunch for now
+    def string_to_datetime(self, date_str):
+
+        try:
+            return self.base.datetime.datetime.strptime(date_str, '%B %d  %Y, %I:%M %p').date()
+        except TypeError:
+            return None
+
+    def read_date_data_dict(self, node):
+        node_data = node['structuredDataNodes']['structuredDataNode']
+        date_data = {}
+        for date in node_data:
+            date_data[date['identifier']] = date['text']
+        # If there is no date, these will fail
+        try:
+            date_data['start-date'] = self.java_unix_to_date(date_data['start-date'])
+        except TypeError:
+            pass
+        try:
+            date_data['end-date'] = self.java_unix_to_date(date_data['end-date'])
+        except TypeError:
+            pass
+
+        return date_data
+
+    def traverse_event_folder(self, traverse_xml, username):
+        # Travserse an XML folder, adding system-pages to a dict of matches
+        # todo use xpath instead of calling this?
+
+        if username == "":
+            matches = []
+            for child in traverse_xml.findall('.//system-page'):
+                author = None
+                if child.find('author'):
+                    author = child.find('author').text
+
+                page_values = {
+                    'author': author,
+                    'id': child.attrib['id'] or None,
+                    'title': child.find('title').text or None,
+                    'created-on': child.find('created-on').text or None,
+                }
+                # This is a match, add it to array
+                matches.append(page_values)
+            return matches
+
+        matches = []
+        for child in traverse_xml.findall('.//system-page'):
+            try:
+                author = child.find('author').text
+            except AttributeError:
+                continue
+
+            try:
+                is_published = child.find('last-published-on').text
+            except AttributeError:
+                is_published = False
+
+            author = author.replace(" ", "")
+            author = author.split(",")
+
+            if username in author:
+                dates = child.find('system-data-structure').findall('event-dates')
+                dates_str = []
+                for date in dates:
+                    # start = int(date.find('start-date').text) / 1000
+                    # end = int(date.find('end-date').text) / 1000
+                    start = int(date.find('start-date').text) / 1000
+                    end = int(date.find('end-date').text) / 1000
+                    dates_str.append(self.friendly_date_range(start, end))
+
+                page_values = {
+                    'author': child.find('author').text,
+                    'id': child.attrib['id'] or None,
+                    'title': child.find('title').text or None,
+                    'created-on': child.find('created-on').text or None,
+                    'path': 'https://www.bethel.edu' + child.find('path').text or None,
+                    'is_published': is_published,
+                    'event-dates': "<br/>".join(dates_str),
+                }
+                # This is a match, add it to array
+                matches.append(page_values)
+
+        return matches
+
+    def event_date(self, start, end, all_day=False):
+
+        date_list = [
+            self.structured_data_node("start-date", start),
+            self.structured_data_node("end-date", end),
+        ]
+        if all_day:
+            date_list.append(self.structured_data_node("all-day", "::CONTENT-XML-CHECKBOX::Yes"))
+
+        node = {
+            'type': "group",
+            'identifier': "event-dates",
+            'structuredDataNodes': {
+                'structuredDataNode': date_list,
+            },
+        },
+
+        return node
+
+    def move_event_year(self, event_id, data):
+        new_path = self.get_event_folder_path(data)
+        resp = self.base.move(event_id, new_path[1])
+        return resp
