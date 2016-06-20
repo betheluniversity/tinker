48530160fa79c5cb647ebabdc3c29a029f70ad11
mjf78594 <mjf78594@bethel.edu>
2016-06-20 10:09:49 -0500

    uncommented previous code to start working on "edit_event_page" function

--- tinker/events/__init__.py
+++ tinker/events/__init__.py
@@ -68,49 +68,49 @@ class EventsView(FlaskView):
         event_block = self.base.read_block(event_id)
         event_data = event_block.read_asset()
 
-        # form_data, s_data = event_block.read_asset()
+        form_data, s_data = event_block.read_asset()
         # Get the event data from cascade
-        # event_data = self.base.read(event_id)
+        event_data = self.base.read(event_id)
         edit_data = self.base.get_edit_data(event_data)
         # Get the different data sets from the response
-        # form_data = event_data.asset.page
-        #
-        # # the stuff from the data def
-        # # s_data = form_data.structuredData.structuredDataNodes.structuredDataNode
-        # s_data = form_data["structuredData"]["structuredDataNodes"]["structuredDataNode"]
-        # # regular metadata
-        # metadata = form_data.metadata
-        # # dynamic metadata
-        # dynamic_fields = metadata.dynamicFields.dynamicField
-        # # This dict will populate our EventForm object
-        # edit_data = {}
-        # date_count = 0
-        # dates = {}
-        # # Start with structuredDataNodes (data def content)
-        # for node in s_data:
-        #     node_identifier = node.identifier.replace('-', '_')
-        #     node_type = node.type
-        #     if node_type == "text":
-        #         edit_data[node_identifier] = node.text
+        form_data = event_data.asset.page
+
+        # the stuff from the data def
+        # s_data = form_data.structuredData.structuredDataNodes.structuredDataNode
+        s_data = form_data["structuredData"]["structuredDataNodes"]["structuredDataNode"]
+        # regular metadata
+        metadata = form_data.metadata
+        # dynamic metadata
+        dynamic_fields = metadata.dynamicFields.dynamicField
+        # This dict will populate our EventForm object
+        edit_data = {}
+        date_count = 0
+        dates = {}
+        # Start with structuredDataNodes (data def content)
+        for node in s_data:
+            node_identifier = node.identifier.replace('-', '_')
+            node_type = node.type
+            if node_type == "text":
+                edit_data[node_identifier] = node.text
         # todo should the date be in the tinker controller?
-        #     elif node_type == 'group':
-        #         # These are the event dates. Create a dict so we can convert to JSON later.
-        #         dates[date_count] = read_date_data_structure(node)
-        #         date_count += 1
-        #     elif node_identifier == 'image':
-        #         edit_data['image'] = node.filePath
-        # #
-        # # now metadata dynamic fields
-        # for field in dynamic_fields:
-        #     # This will fail if no metadata is set. It should be required but just in case
-        #     if field.fieldValues:
-        #         items = [item.value for item in field.fieldValues.fieldValue]
-        #         edit_data[field.name.replace('-', '_')] = items
+            elif node_type == 'group':
+                # These are the event dates. Create a dict so we can convert to JSON later.
+                dates[date_count] = read_date_data_structure(node)
+                date_count += 1
+            elif node_identifier == 'image':
+                edit_data['image'] = node.filePath
+        #
+        # now metadata dynamic fields
+        for field in dynamic_fields:
+            # This will fail if no metadata is set. It should be required but just in case
+            if field.fieldValues:
+                items = [item.value for item in field.fieldValues.fieldValue]
+                edit_data[field.name.replace('-', '_')] = items
 
         # Add the rest of the fields. Can't loop over these kinds of metadata
-        # edit_data['title'] = metadata.title
-        # edit_data['teaser'] = metadata.metaDescription
-        # author = metadata.author
+        edit_data['title'] = metadata.title
+        edit_data['teaser'] = metadata.metaDescription
+        author = metadata.author
 
         # Create an EventForm object with our data
         form = EventForm(**edit_data)
