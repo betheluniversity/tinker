f4a0f7f7c0e144ffd8a9ee51ce24e865b9f440e8
josiahtillman <jot43536@bethel.edu>
2016-07-07 16:34:49 -0500

    Refactoring of the submit_form(incomplete)

--- tinker/events/__init__.py
+++ tinker/events/__init__.py
@@ -59,6 +59,9 @@ class EventsView(FlaskView):
             return redirect(url_for('events.EventsView:event_in_workflow'), code=302)
 
         edit_data, form, dates, author = self.base.build_edit_form(event_id)
+        # todo convert 'On/Off campus' to 'On/Off Campus' for all events
+        if edit_data['location']:
+            edit_data['location'].replace(' c', ' C')
 
         return render_template('event-form.html', **locals())
 
@@ -78,43 +81,34 @@ class EventsView(FlaskView):
         workflow = None
         eid = rform.get('event_id')
         # workflow = self.base.get_event_publish_workflow(title, username)
-
         event_dates, dates_good, num_dates = self.base.check_event_dates(rform)
         failed = self.base.validate_form(rform, dates_good)
         if failed:
             return failed
 
-        # Get all the form data
-        add_data = self.base.get_add_data(metadata_list, rform)
-        dates = self.base.get_dates(add_data)
-        add_data['event-dates'] = dates
+        if not eid:
+            bid = app.config['EVENTS_BASE_ASSET']
+            event_data, metadata, structured_data = self.base.cascade_connector.load_base_asset_by_id(bid, 'page')
+            # Get all the form data
+            add_data = self.base.get_add_data(metadata_list, rform)
+            dates = self.base.get_dates(add_data)
 
-        page = self.base.read_page(eid)
-        event_data, metadata, structured_data = page.read_asset()
+            add_data['event-dates'] = dates
+            asset = self.base.get_event_structure(event_data, metadata, structured_data, add_data, username, workflow=workflow)
+            response = self.base.create(asset)
 
-        asset = self.base.get_event_structure(event_data, metadata, structured_data, add_data, username, workflow=workflow)
-        response = self.base.create(asset)
+        else:
+            page = self.base.read_page(eid)
+            # Get all the form data
+            add_data = self.base.get_add_data(metadata_list, rform)
+            dates = self.base.get_dates(add_data)
 
-        if username == 'amf39248':
-            app.logger.debug(time.strftime("%c") + ": TESTING" + asset)
-            app.logger.debug(time.strftime("%c") + ": TESTING" + response)
-
-        # Checks if the link is valid
-        if 'link' in add_data and add_data['link'] != "":
-            from tinker.admin.redirects import new_internal_redirect_submit
-            path = str(asset['page']['parentFolderPath'] + "/" + asset['page']['name'])
-            new_internal_redirect_submit(path, add_data['link'])
-
-        if edit:
-            form = rform
+            add_data['event-dates'] = dates
             add_data['author'] = request.form['author']
-            event_id = form['event_id']
-
+            event_id = rform['event_id']
             asset = self.base.get_event_structure(add_data, username, workflow=workflow, event_id=event_id)
-
             current_year = self.base.get_current_year_folder(event_id)
             new_year = self.base.get_year_folder_value(add_data)
-
             proxy_page = self.base.read_page(event_id)
             response = proxy_page.edit_asset(asset)
             self.base.log_sentry("Event edit submission", response)
@@ -122,6 +116,12 @@ class EventsView(FlaskView):
             if new_year > current_year:
                 response = self.base.move_event_year(event_id, add_data)
                 app.logger.debug(time.strftime("%c") + ": Event move submission by " + username + " " + str(response))
+
+        # Checks if the link is valid
+        if 'link' in add_data and add_data['link'] != "":
+            from tinker.admin.redirects import new_internal_redirect_submit
+            path = str(asset['page']['parentFolderPath'] + "/" + asset['page']['name'])
+            new_internal_redirect_submit(path, add_data['link'])
 
         return redirect(url_for('events.EventsView:confirm'), code=302)
 
