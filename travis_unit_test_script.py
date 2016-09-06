15991c08c5aa5f0ec6b275bfdf19b617fcece6c8
Philip Gibbens <phg49389@bethel.edu>
2016-09-06 13:48:55 -0500

    Travis 22

--- travis_unit_test_script.py
+++ travis_unit_test_script.py
@@ -1,6 +1,6 @@
 import sys
 import unittest
 
-testsuite = unittest.TestLoader().discover('/unit_tests')
+testsuite = unittest.TestLoader().discover('unit_tests')
 runner = unittest.TextTestRunner(verbosity=1).run(testsuite)
 sys.exit(len(runner.failures))
