322d3a9376a50dfde8028cd4122f6488bcd5b82c
Philip Gibbens <phg49389@bethel.edu>
2016-09-26 16:22:06 -0500

    Bug-fix for tinker.xp
    Sync was pulling code mid-testing, and it was breaking subsequent tests.

--- tinker/admin/sync/__init__.py
+++ tinker/admin/sync/__init__.py
@@ -33,7 +33,8 @@ class SyncView(FlaskView):
     @route("/all", methods=['post'])
     def all(self):
         # get the most recent code
-        self.base.git_pull()
+        # todo: this will need to be added back in. but it currently breaks on xp (since its using a different branch)
+        # self.base.git_pull()
 
         data = data_to_add
         returned_keys = []
