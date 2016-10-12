57f745a6742e3c4cbb4a7a1ee6b8b260e8f6b3cf
Philip Gibbens <phg49389@bethel.edu>
2016-10-12 17:47:48 -0500

    Refactored tinker/admin/redirects so that all db usages go through the controller
    This way, the db reference is only used once, allowing for easier loose
    coupling between the code and whatever db we might want it to use.

--- tinker/admin/redirects/redirects_controller.py
+++ tinker/admin/redirects/redirects_controller.py
@@ -1,10 +1,21 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
 # tinker
-from tinker import app, db
-from tinker.tinker_controller import TinkerController
+from tinker import app
 from tinker.admin.redirects.models import BethelRedirect
+from tinker.tinker_controller import TinkerController
 
 
 class RedirectsController(TinkerController):
+
+    def __init__(self, database):
+        super(RedirectsController, self).__init__()
+        if isinstance(database, SQLAlchemy):
+            self.db = database
+        else:
+            raise TypeError('tinker/admin/redirects/redirects_controller requires that the database arg is SQLAlchemy')
+
     # Creates a new redirect text file
     def create_redirect_text_file(self):
         map_file = open(app.config['REDIRECTS_FILE_PATH'], 'w')
@@ -15,15 +26,47 @@ class RedirectsController(TinkerController):
             map_file.write("%s %s\n" % (item.from_path, item.to_url))
             map_file_back.write("%s %s\n" % (item.from_path, item.to_url))
 
-        resp = 'done'
-        return resp
+        return 'done'
 
-    # Adds the redirect to the database
-    def database_add(self, redirect):
-        db.session.add(redirect)
-        db.session.commit()
+    def get_model(self):
+        return self.db.Model
 
-    # Deletes the redirect to the database
-    def database_delete(self, redirect):
-        db.session.delete(redirect)
-        db.session.commit()
+    def add_row_to_db(self, from_path, to_url, short_url, expiration_date):
+        new_redirect = BethelRedirect(from_path=from_path, to_url=to_url, short_url=short_url,
+                                      expiration_date=expiration_date)
+        self.db.session.add(new_redirect)
+        self.db.session.commit()
+        return new_redirect
+
+    def api_add_row(self, from_path, to_url):
+        new_redirect = BethelRedirect(from_path=from_path, to_url=to_url)
+        self.db.session.add(new_redirect)
+        self.db.session.commit()
+        return new_redirect
+
+    def search_db(self, search_type, term):
+        term += "%"
+        if search_type == "from_path":  # Search by from_path
+            results = BethelRedirect.query.filter(BethelRedirect.from_path.like(term)).limit(100).all()
+        else:  # Search by to_url
+            results = BethelRedirect.query.filter(BethelRedirect.to_url.like(term)).limit(100).all()
+        results.sort()
+        return results
+
+    def delete_row_from_db(self, from_path):
+        redirect_to_delete = BethelRedirect.query.get(from_path)
+        self.db.session.delete(redirect_to_delete)
+        self.db.session.commit()
+
+    def expire_old_redirects(self):
+        today = datetime.utcnow()
+        redirects = BethelRedirect.query.filter(BethelRedirect.expiration_date < today).all()
+        for redirect in redirects:
+            self.db.session.delete(redirect)
+        self.db.session.commit()
+
+    def get_all_rows(self):
+        return BethelRedirect.query.all()
+
+    def rollback(self):
+        self.db.session.rollback()
