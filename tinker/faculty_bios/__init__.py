4f73fab5fffee8bf9a7033410e14e4650418ae27
c-schwarze <ces55739@bethel.edu>
2016-10-14 16:04:33 -0500

    Updated faculty bios to work with new permissions

--- tinker/faculty_bios/__init__.py
+++ tinker/faculty_bios/__init__.py
@@ -24,7 +24,12 @@ class FacultyBiosView(FlaskView):
 
     # todo: add a before_request method
     def before_request(self, name, **kwargs):
-        if 'FACULTY' not in session['roles'] and 'Tinker Faculty Bios' not in session['groups'] and 'Administrators' not in session['groups']:
+        if 'FACULTY' not in session['roles'] \
+                and 'Tinker Faculty Bios - CAS' not in session['groups'] \
+                and 'Tinker Faculty Bios - CAPS and GS' not in session['groups'] \
+                and 'Tinker Faculty Bios - SEM' not in session['groups'] \
+                and 'Administrators' not in session['groups'] \
+                and 'Tinker Faculty Bios - Admin' not in session['groups']:
             abort(403)
 
     def index(self):
@@ -35,7 +40,7 @@ class FacultyBiosView(FlaskView):
         forms = sorted(forms, key=itemgetter('last-name'), reverse=False)
 
         # the faculty special admins should be able to see every bio, based on school.
-        if username in app.config['FACULTY_BIOS_ADMINS']:
+        if 'Tinker Faculty Bios - Admin' in session['groups'] or 'Administrators' in session['groups']:
             show_special_admin_view = True
             show_create = True
 
@@ -50,7 +55,7 @@ class FacultyBiosView(FlaskView):
             ]
         else:  # normal view
             show_special_admin_view = False
-            show_create = len(forms) == 0 or 'Tinker Faculty Bios' in session['groups']
+            show_create = len(forms) == 0 or 'Tinker Faculty Bios - CAS' in session['groups'] or 'Tinker Faculty Bios - CAPS and GS' in session['groups'] or 'Tinker Faculty Bios - SEM' in session['groups']
 
         return render_template('faculty-bio-home.html', **locals())
 
