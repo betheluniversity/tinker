b877186791bcb3ee2909ab070804ac2c46384e9d
c-schwarze <ces55739@bethel.edu>
2016-12-14 15:46:42 -0600

    tsdfasf

--- tinker/e_announcements/campaign_controller.py
+++ tinker/e_announcements/campaign_controller.py
@@ -1,4 +1,7 @@
 import datetime
+from dateutil.easter import *
+
+import math
 from createsend import *
 
 # flask
@@ -50,11 +53,54 @@ class CampaignController(TinkerController):
 
         # Check if date is between 12/24 and 1/1
         dates_to_ignore = ['12/24', '12/25', '12/26', '12/27', '12/28', '12/29', '12/30', '12/31', '1/1']
+        holidays = self.is_bethel_holiday(date)
         current_month_day = str(date.month) + '/' + str(date.day)
         if current_month_day in dates_to_ignore:
             return False
 
         return True
+
+    def is_bethel_holiday(self, date):
+        # new years
+        if date.month == 1 and date.day == 1:
+            return True
+        # bonus new years day
+        elif date.month == 1 and date.weekday == 1 and date.day == 2:
+            return True
+        # MLK - 3rd monday in jan
+        elif date.month == 1 and date.weekday == 1 and math.ceil(date.day/7) == 3:
+            return True
+        # Easter
+        elif self.is_friday_before_easter(date):
+            return True
+        # memorial day - last monday in may (may, date is after 24th and its a monday)
+        elif date.month == 5 and date.weekday == 1 and date.day >= 24:
+            return True
+        # july 4
+        elif date.month == 7 and date.day == 4:
+            return True
+        # labor day - first monday in sept
+        elif date.month == 9 and date.weekday == 1 and math.ceil(date.day/7) == 1:
+            return True
+        # Thanksgiving and Black Friday - 4th thursday/friday in november
+        elif date.month == 11 and math.ceil(date.day/7) == 4 and (date.weekday == 4 or date.weekday == 5):
+            return True
+        # bonus Christmas eve day -- if christmas eve is on a Saturday, we get the Friday off
+        elif date.month == 12 and date.weekday == 5 and date.day == 23:
+            return True
+        # christmas on
+        elif date.month == 12 and date.day >= 24:
+            return True
+
+        return False
+
+    def is_friday_before_easter(self, date):
+        easter_day = easter(date.year)
+
+        if date.month == easter_day.month and easter_day.weekday - 2 == date.day:
+            return True
+
+        return False
 
     def get_layout_for_no_announcements(self, roles):
         if_block = ''
