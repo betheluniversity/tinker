db3208bbbc173bb0eaf8d3940204a8a8f424489b
Philip Gibbens <phg49389@bethel.edu>
2017-02-17 11:52:33 -0600

    First commit in the unit tests' refactor

--- unit_tests/admin/program_search_tests/test_program_search_search.py
+++ unit_tests/admin/program_search_tests/test_program_search_search.py
@@ -13,7 +13,7 @@ class SearchTestCase(ProgramSearchBaseTestCase):
         self.request_type = "POST"
         self.request = self.generate_url("search")
 
-    def create_form(self, search_key, search_tag):
+    def create_form(self, search_key="theatre", search_tag=""):
         return json.dumps({
             "search_key": search_key,
             "search_tag": search_tag
@@ -25,8 +25,21 @@ class SearchTestCase(ProgramSearchBaseTestCase):
 
     def test_search_valid(self):
         expected_response = b'class="program-search-row table-hover">'
-        form_contents = self.create_form("theatre", "")
+        form_contents = self.create_form()
         response = self.send_post(self.request, form_contents)
         failure_message = self.generate_failure_message(self.request_type, self.request, response.data,
                                                         expected_response, self.class_name, self.get_line_number())
         self.assertIn(expected_response, response.data, msg=failure_message)
+
+    def test_search_invalid(self):
+        expected_response = ""
+        arg_names = ['search_key', 'search_tag']
+        for i in range(len(arg_names)):
+            bad_arg = {arg_names[i]: None}
+            form = self.create_form(**bad_arg)
+            response = self.send_post(self.request, form)
+            failure_message = self.generate_failure_message(self.request_type, self.request, response.data,
+                                                            expected_response,
+                                                            self.class_name + "/search_invalid_" + arg_names[i],
+                                                            self.get_line_number())
+            self.assertIn(expected_response, response.data, msg=failure_message)
