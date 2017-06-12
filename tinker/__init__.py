6aaa76f403b63e9ba892d46a09d414f8ed4b9a21
josiahtillman <jot43536@bethel.edu>
2017-06-12 14:57:31 -0500

    Moved url to config

--- tinker/__init__.py
+++ tinker/__init__.py
@@ -140,7 +140,7 @@ def before_request():
 @app.route("/logout", methods=["GET"])
 def logout():
     session.clear()
-    resp = make_response(redirect("https://auth.bethel.edu/cas/logout"))
+    resp = make_response(redirect(app.config['LOGOUT_URL']))
     resp.set_cookie('MOD_AUTH_CAS_S', '', expires=0)
     resp.set_cookie('MOD_AUTH_CAS', '', expires=0)
     return resp
