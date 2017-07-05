2017c01a2932f9a44cd37122f26fa7bbcc0c003e
bak45247 <bak45247@bethel.edu>
2017-07-05 14:22:33 -0500

    more unnecessary deletions

--- tinker/admin/admin_permissions.py
+++ tinker/admin/admin_permissions.py
@@ -1,16 +1,20 @@
 from flask import session, abort, request
 
 
+# *args because some of the menus are called with args and some with **kwargs
 def admin_permissions(self, route_base, *args):
+    # this handle everything but redirects and the program search admin menus
     if route_base != '/admin/redirect' or route_base != '/admin/program-search':
         if 'Administrators' not in session['groups']:
             abort(403)
 
+    # program search menu
     if route_base == '/admin/program-search':
         # give access to admins and lauren
         if 'Administrators' not in session['groups'] and 'parlau' not in session['groups'] and session['username'] != 'kaj66635':
             abort(403)
 
+    # redirect menu
     if route_base == '/admin/redirect':
         if '/public/' in request.path:
             return
