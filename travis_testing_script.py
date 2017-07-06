c24a17a24fa4ea085c32a39cc069267cf3bfab52
Philip Gibbens <phg49389@bethel.edu>
2017-07-06 13:20:09 -0500

    Split up the Travis CI script into two distinct test runs
    The first is for unit tests, the second is for integration tests.

--- travis_testing_script.py
+++ travis_testing_script.py
@@ -3,6 +3,16 @@ import unittest
 
 from testing_suite.utilities import get_tests_in_this_dir
 
-testsuite = get_tests_in_this_dir('testing_suite')
-runner = unittest.TextTestRunner(verbosity=1).run(testsuite)
-sys.exit(len(runner.failures) + len(runner.errors))
+
+# Run unit tests first
+print 'UNIT TESTS\n'
+unit_tests = get_tests_in_this_dir('testing_suite/unit_tests')
+unit_tests_run = unittest.TextTestRunner(verbosity=1).run(unit_tests)
+
+# Run integration tests second
+print 'INTEGRATION TESTS\n'
+integration_tests = get_tests_in_this_dir('testing_suite/integration_tests')
+integration_tests_run = unittest.TextTestRunner(verbosity=1).run(integration_tests)
+
+sys.exit(len(unit_tests_run.failures) + len(unit_tests_run.errors) +
+         len(integration_tests_run.failures) + len(integration_tests_run.errors))
