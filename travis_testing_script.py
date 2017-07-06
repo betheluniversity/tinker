f8ff535d07103b5b818d763c4bf67cdde8f3a990
Philip Gibbens <phg49389@bethel.edu>
2017-07-06 13:47:23 -0500

    Reverted Travis CI script to only have one run again

--- travis_testing_script.py
+++ travis_testing_script.py
@@ -3,14 +3,7 @@ import unittest
 
 from testing_suite.utilities import get_tests_in_this_dir
 
+all_tests = get_tests_in_this_dir('testing_suite')
+runner = unittest.TextTestRunner(verbosity=1).run(all_tests)
 
-# Run unit tests first
-unit_tests = get_tests_in_this_dir('./testing_suite/unit_tests')
-unit_tests_run = unittest.TextTestRunner(verbosity=1).run(unit_tests)
-
-# Run integration tests second
-integration_tests = get_tests_in_this_dir('./testing_suite/integration_tests')
-integration_tests_run = unittest.TextTestRunner(verbosity=1).run(integration_tests)
-
-sys.exit(len(unit_tests_run.failures) + len(unit_tests_run.errors) +
-         len(integration_tests_run.failures) + len(integration_tests_run.errors))
+sys.exit(len(runner.failures) + len(runner.errors))
