a9f1db5411ae06015d8b64f8ec6caeea67c91fc2
Philip Gibbens <phg49389@bethel.edu>
2017-07-06 13:34:01 -0500

    Changing style of paths to testing folders

--- travis_testing_script.py
+++ travis_testing_script.py
@@ -5,13 +5,11 @@ from testing_suite.utilities import get_tests_in_this_dir
 
 
 # Run unit tests first
-print 'UNIT TESTS\n'
-unit_tests = get_tests_in_this_dir('testing_suite/unit_tests')
+unit_tests = get_tests_in_this_dir('./testing_suite/unit_tests')
 unit_tests_run = unittest.TextTestRunner(verbosity=1).run(unit_tests)
 
 # Run integration tests second
-print 'INTEGRATION TESTS\n'
-integration_tests = get_tests_in_this_dir('testing_suite/integration_tests')
+integration_tests = get_tests_in_this_dir('./testing_suite/integration_tests')
 integration_tests_run = unittest.TextTestRunner(verbosity=1).run(integration_tests)
 
 sys.exit(len(unit_tests_run.failures) + len(unit_tests_run.errors) +
