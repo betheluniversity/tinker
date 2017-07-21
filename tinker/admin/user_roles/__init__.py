0b7132e5b256cdef5be2abf57eac197af277665b
bak45247 <bak45247@bethel.edu>
2017-07-21 09:59:52 -0500

    reworked the page it redirects to

--- tinker/admin/user_roles/__init__.py
+++ tinker/admin/user_roles/__init__.py
@@ -30,6 +30,19 @@ class UserRolesView(FlaskView):
         admin_permissions(self)
 
     def index(self):
+        cascade_connection = Cascade(app.config['SOAP_URL'],
+                                     {'username': app.config['AUTH_USERNAME'], 'password': app.config['AUTH_PASSWORD']},
+                                     app.config['USER_ROLES_SITE_ID'], app.config['USER_ROLES_STAGING_DESTINATION_ID'])
+        role_asset = cascade_connection.read(app.config['CASCADE_MD_ROLES_ID'], 'metadataset')
+        role_data = role_asset['asset']['metadataSet']['dynamicMetadataFieldDefinitions'][
+            'dynamicMetadataFieldDefinition']
+
+        cascade_md_roles = {}
+        for item in role_data:
+            try:
+                cascade_md_roles[item['name']] = item['possibleValues']['possibleValue']
+            except:
+                continue
         return render_template('user_roles_home.html', **locals())
 
     def load_user_roles(self):
@@ -38,19 +51,38 @@ class UserRolesView(FlaskView):
             else:
                 session['user_roles'] = [role.upper() for role in app.config['ROLES']]
 
-    def test_roles_and_user(self):
-        cascade_connection = Cascade(app.config['SOAP_URL'],{'username': app.config['AUTH_USERNAME'], 'password': app.config['AUTH_PASSWORD']},
-                                         app.config['SITE_ID'], app.config['STAGING_DESTINATION_ID'])
-        role_asset = cascade_connection.read(app.config['CASCADE_MD_ROLES_ID'], 'metadataset')
-        role_data = role_asset['asset']['metadataSet']['dynamicMetadataFieldDefinitions']['dynamicMetadataFieldDefinition']
+    @route('/test_roles_and_users_submit/', methods=['POST'])
+    def test_roles_and_users_submit(self):
+        if 'admin_username' in session.keys():
+            current_username = session['admin_viewer_username']
+        else:
+            current_username = session['username']
 
-        cascade_md_roles = {}
-        for item in role_data:
-            try:
-                cascade_md_roles[item['name']] = item['possibleValues']['possibleValue']
-            except:
-                continue
+        # get roles
+        role = request.form.get('role')
+        # get username
+        username = request.form.get('username')
 
-        return render_template('test_roles_and_users.html', **locals())
+        # Todo: the main problem with doing role based checks, is some channels require a username
+        # Todo: also, things like the profile image are up in the air. do we show the user's image? or hide it.
+        if role:
+            # set user_roles and clear user_tabs
+            session['admin_viewer_role'] = role
+            session['user_roles'] = [role]
+            session.pop('user_tabs')
+        elif username:
+            session.clear()
+            session['admin_username'] = current_username
+            session['username'] = username
+        else:
+            return 'error'
+
+        session['admin_viewer'] = True
+        return '/'  # have the JS handle where we go (homepage)
+
+    @route('/test_roles_and_users_remove/')
+    def test_roles_and_users_remove(self):
+        session.clear()
+        return 'success'
 
 UserRolesView.register(UserRolesBlueprint)
