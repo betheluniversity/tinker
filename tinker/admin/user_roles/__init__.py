43fdf0244269d4d3d0f9666199ede5860bfdfce9
c-schwarze <ces55739@bethel.edu>
2017-07-25 15:22:01 -0500

    fix

--- tinker/admin/user_roles/__init__.py
+++ tinker/admin/user_roles/__init__.py
@@ -76,7 +76,7 @@ class UserRolesView(FlaskView):
             return 'error'
 
         session['admin_viewer'] = True
-        return session['admin_viewer']  # have the JS handle where we go (homepage)
+        return str(session['admin_viewer'])  # have the JS handle where we go (homepage)
 
     @route('/test_roles_and_users_remove/')
     def test_roles_and_users_remove(self):
