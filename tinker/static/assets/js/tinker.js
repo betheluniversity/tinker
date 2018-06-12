b17a67c01938b1c2152b76b19ad089717449abac
Jordan Jasik <joj28267@bethel.edu>
2018-06-12 09:13:57 -0500

    Pulled JS to External File
    Cleaned up the additional search filters and pulled out some common code between e-announcements and events to an external js file called tinker.js

--- tinker/static/assets/js/tinker.js
+++ tinker/static/assets/js/tinker.js
@@ -1,1 +1,16 @@
 // General File for Tinker's JavaScript
+
+$(document).ready(function () {
+    $("#search-filters").click(function () {
+        if($("#search-filters").hasClass("show")) {
+            $("#additional-search-params").slideUp();
+            $("#search-filters").removeClass("show");
+            $("#search-filters").addClass("no-show");
+            $(".input-field").val(""); // Clears values if hidden so not included in search
+        }else{
+            $("#additional-search-params").slideDown();
+            $("#search-filters").removeClass("no-show");
+            $("#search-filters").addClass("show");
+        }
+    });
+});
