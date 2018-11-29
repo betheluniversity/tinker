bc9476eebf72209d33bcae713d4e26807492016b
Bailey Middendorf <bam95899@bethel.edu>
2018-11-29 13:20:18 -0600

    Compressed js and moved to tinker.js so both locations can access it

--- tinker/static/assets/js/tinker.js
+++ tinker/static/assets/js/tinker.js
@@ -15,3 +15,122 @@ $(document).ready(function () {
         }
     });
 });
+
+function pagination() {
+
+    var numberOfItems = $(" #loop .list-group").length;
+    var limitPerPage = $("#first").val();
+
+    $("#selected-option").change(function() {
+        var o = $(this).find("option:selected");
+        if (o.val() === "All") {
+            limitPerPage = numberOfItems;
+        } else {
+            limitPerPage = o.val()
+        }
+
+        $(".pagination").children("li.to-remove").remove();
+
+        var totalPages = Math.ceil(numberOfItems / limitPerPage);
+
+        createButtons(totalPages, limitPerPage);
+
+        var currentPage = 1;
+
+        var grandTotal = limitPerPage * currentPage;
+
+        for (var i = grandTotal - limitPerPage; i < grandTotal ; i ++) {
+            $("#loop .list-group:eq(" + i + ")").show();
+        }
+
+        switchPages(limitPerPage);
+
+        nextPage(totalPages, limitPerPage);
+
+    });
+
+    var totalPages = Math.ceil(numberOfItems / limitPerPage);
+
+    createButtons(totalPages, limitPerPage);
+
+    switchPages(limitPerPage);
+
+    nextPage(totalPages, limitPerPage);
+
+    previousPage(totalPages, limitPerPage);
+}
+
+function nextPage(totalPages, limitPerPage) {
+    $("#next-page").on("click", function() {
+        var currentPage = $(".pagination li.active").index();
+        if (currentPage === totalPages) {
+            return false;
+        } else {
+            currentPage ++;
+            $(".pagination li").removeClass("active");
+            $("#loop .list-group").hide();
+
+            var grandTotal = limitPerPage * currentPage;
+
+            for (var i = grandTotal - limitPerPage; i < grandTotal ; i ++) {
+                $("#loop .list-group:eq(" + i + ")").show();
+            }
+
+            $(".pagination li.current-page:eq(" + (currentPage - 1) + ")").addClass("active");
+
+        }
+
+    });
+}
+
+function previousPage(totalPages, limitPerPage) {
+    $("#previous-page").on("click", function() {
+        var currentPage = $(".pagination li.active").index();
+        if (currentPage === 1) {
+             return false;
+        } else {
+             currentPage --;
+             $(".pagination li").removeClass('active');
+             $("#loop .list-group").hide();
+
+             var grandTotal = limitPerPage * currentPage;
+
+             for (var i = grandTotal - limitPerPage; i < grandTotal; i++) {
+                 $("#loop .list-group:eq(" + i + ")").show();
+             }
+
+             $(".pagination li.current-page:eq(" + (currentPage - 1) + ")").addClass('active');
+         }
+    });
+}
+
+function switchPages(limitPerPage) {
+    $(".pagination li.current-page").on("click", function() {
+        if ($(this).hasClass("active")) {
+            return false;
+        } else {
+            var currentPage = $(this).index();
+            $(".pagination li").removeClass("active");
+            $(this).addClass("active");
+            $("#loop .list-group").hide();
+
+            var grandTotal = limitPerPage * currentPage;
+
+            for (var i = grandTotal - limitPerPage; i < grandTotal ; i ++) {
+                $("#loop .list-group:eq(" + i + ")").show();
+            }
+        }
+    });
+}
+
+function createButtons(totalPages, limitPerPage) {
+    $(" #loop .list-group:gt(" + (limitPerPage - 1) + ")").hide();
+
+    $(".pagination").append("<li class='current-page active to-remove'><a href='javascript:void(0)'>" + 1 + "</a></li>");
+
+    for (var i = 2; i <= totalPages; i++){
+        $(".pagination").append("<li class='current-page to-remove'><a href='javascript:void(0)'>" + i + "</a></li>")
+    }
+
+    $(".pagination").append("<li id='next-page' class='to-remove'><a href='javascript:void(0)' aria-label='Next'><span aria-hidden='true'>&raquo;</span></a></li>");
+}
