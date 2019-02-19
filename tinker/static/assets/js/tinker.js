47756f9ceff8df89806d4dd8e0e6439b4bfdd140
Bailey Middendorf <bam95899@bethel.edu>
2019-02-19 13:27:15 -0600

    fixed goToPage method and fixed a few bugs
    Also changed vars to lets so that the variables are block-scoped so it leaves no possiblity that variables could be changed in different functions

--- tinker/static/assets/js/tinker.js
+++ tinker/static/assets/js/tinker.js
@@ -17,11 +17,17 @@ $(document).ready(function () {
 });
 
 
-var paginationRange = 10;
+const paginationRange = 10;
+// maxPages is up here since it looks like a lot of variables were getting confused as to what the variable "totalPages"
+// was suppose to be set to since it always used the old version after the limit was changed. So in order to fix this
+// issue, I made this variable "maxPages" with overseeing scope and whenever "totalPages" changes I set "maxPages" equal
+// to it and just hand the functions that need "totalPages" the "maxPage" variable.
+// ~~~~~~~~~~~~~~~~ For a specific example look at the comments of the function goToPage(limitPerPage) ~~~~~~~~~~~~~~~~~
+let maxPages = 0;
 function pagination(type) {
-    var numberOfItems = $(" #loop .items-to-paginate").length;
+    let numberOfItems = $(" #loop .items-to-paginate").length;
     if (numberOfItems > 10) {
-        var limitPerPage = 10;
+        let limitPerPage = 10;
         if (type == 'e-announcement') {
             if (Cookies.get('e-announcement-cookie') != null) {
                 limitPerPage = Cookies.get('e-announcement-cookie');
@@ -36,9 +42,9 @@ function pagination(type) {
             }
         }
 
-        var limitList = [10, 25, 50, numberOfItems]
+        let limitList = [10, 25, 50, numberOfItems]
 
-        for (var i = 0; i < limitList.length; i ++) {
+        for (let i = 0; i < limitList.length; i ++) {
             if (limitList[i] == limitPerPage) {
                 if (limitList[i] == numberOfItems) {
                      $("#selected-option").append("<option value=\" " + limitList[i] + " \" selected='selected'>All</option>");
@@ -64,45 +70,48 @@ function pagination(type) {
 
             $(".pagination").children("li.temp-button").remove();
 
-            var totalPages = Math.ceil(numberOfItems / limitPerPage);
+            let totalPages = Math.ceil(numberOfItems / limitPerPage);
+            maxPages = totalPages;
 
             createButtons(totalPages, limitPerPage);
 
             // When a new limit is selected, we reset the current page to one, since if we have 10 pages, and then
             // we changed to only having one page, we can't keep whatever page we were previously on (if it was greater
             // than 1) so we instead just set it to 1.
-            var currentPage = 1;
+            let currentPage = 1;
 
-            var grandTotal = limitPerPage * currentPage;
+            let grandTotal = limitPerPage * currentPage;
 
-            for (var i = grandTotal - limitPerPage; i < grandTotal ; i ++) {
+            for (let i = grandTotal - limitPerPage; i < grandTotal ; i ++) {
                 $("#loop .items-to-paginate:eq(" + i + ")").show();
             }
 
-            switchPageClick(limitPerPage, totalPages);
+            switchPageClick(limitPerPage);
 
-            nextOrPreviousPage(totalPages, limitPerPage, "next-page");
+            nextOrPreviousPage(limitPerPage, "next-page");
 
-            goToPage(limitPerPage, totalPages);
+            goToPage(limitPerPage);
         });
 
-        var totalPages = Math.ceil(numberOfItems / limitPerPage);
+        let totalPages = Math.ceil(numberOfItems / limitPerPage);
+        maxPages = totalPages;
 
-        createButtons(totalPages, limitPerPage);
+        createButtons(limitPerPage);
 
-        switchPageClick(limitPerPage, totalPages);
+        switchPageClick(limitPerPage);
 
-        nextOrPreviousPage(totalPages, limitPerPage, "next-page");
+        nextOrPreviousPage(limitPerPage, "next-page");
 
-        nextOrPreviousPage(totalPages, limitPerPage, "previous-page");
+        nextOrPreviousPage(limitPerPage, "previous-page");
 
-        goToPage(limitPerPage, totalPages);
+        goToPage(limitPerPage);
     }
 }
 // Sets the next or previous page depending on what button you pressed
-function nextOrPreviousPage(totalPages, limitPerPage, type) {
+function nextOrPreviousPage(limitPerPage, type) {
     $("#" + type + " ").on("click", function () {
-        var currentPage = $(".pagination li.active").index();
+        let currentPage = $(".pagination li.active").index();
+        let totalPages = maxPages;
 
         if (totalPages > paginationRange + 1) {
             // Increments or decrements the currentPage depending on which button was pressed
@@ -142,7 +151,7 @@ function nextOrPreviousPage(totalPages, limitPerPage, type) {
                 }
             }
             $(".pagination li.current-page:eq(" + (currentPage - 1) + ")").addClass("active");
-            switchPages(limitPerPage, totalPages, currentPage, type);
+            switchPages(limitPerPage, currentPage, type);
         } else {
             if (type === "next-page") {
                 if (currentPage === totalPages) {
@@ -160,9 +169,9 @@ function nextOrPreviousPage(totalPages, limitPerPage, type) {
             $(".pagination li").removeClass("active");
             $("#loop .items-to-paginate").hide();
 
-            var grandTotal = limitPerPage * currentPage;
+            let grandTotal = limitPerPage * currentPage;
 
-            for (var i = grandTotal - limitPerPage; i < grandTotal ; i ++) {
+            for (let i = grandTotal - limitPerPage; i < grandTotal ; i ++) {
                 $("#loop .items-to-paginate:eq(" + i + ")").show();
             }
             $(".pagination li.current-page:eq(" + (currentPage - 1) + ")").addClass("active");
@@ -170,15 +179,20 @@ function nextOrPreviousPage(totalPages, limitPerPage, type) {
     });
 }
 
-// TODO GOTTA FIX THIS METHOD SINCE IT WORKS WITH PREVIOUS VALUES WHEN CHANGING LIMIT
 // This method changes the page when you input a page number and click "Go"
-function goToPage(limitPerPage, totalPages) {
+// I had an error before when my function header was "goToPage(limitPerPage, totalPages)" that whenever the limit
+// was changed, the "totalPages" variable would still be set to the old one, thus making it possible to go to pages
+// that weren't valid for the new limit. I overcame this by creating a variable called "maxPages" which is at a overseeing
+// scope level over the pagination function and whenever the "totalPages" variable changes, "maxPages" is set to it and
+// then we can just use "maxPages" here in alternative to "totalPages"
+function goToPage(limitPerPage) {
     $("button#go-to-button").on("click", function() {
-        var page = document.getElementById("go-to-input");
-        var value = Math.floor(page.value);
+        let totalPages = maxPages;
+        let page = document.getElementById("go-to-input");
+        let value = Math.floor(page.value);
         if (value > 0 && value < totalPages + 1 && totalPages > paginationRange) {
             $(".pagination li").removeClass("active");
-            var currentPage = value;
+            let currentPage = value;
             if (currentPage < 2) {
                 currentPage = currentPage - 1;
             } else if (currentPage == totalPages) {
@@ -187,60 +201,58 @@ function goToPage(limitPerPage, totalPages) {
                 currentPage = currentPage + 1;
             }
             $(".pagination li.current-page:eq(" + (currentPage) + ")").addClass("active");
-            switchPages(limitPerPage, totalPages, Math.floor(page.value), "other");
+            switchPages(limitPerPage, Math.floor(page.value), "other");
             page.value = "";
         } else if (value > 0 && value < totalPages + 1) {
             $(".pagination li").removeClass("active");
             $(".pagination li.current-page:eq(" + (value - 1) + ")").addClass("active");
             page.value = "";
         } else {
-            // TODO MAYBE THROW AN ERROR
-            return;
+            return ;
         }
     });
 }
 
 // This method switches the page when you click on a button
-function switchPageClick(limitPerPage, totalPages) {
+function switchPageClick(limitPerPage) {
     $(".pagination li.current-page").on("click", function() {
         if ($(this).hasClass("active")) {
             return false;
         } else {
-            var currentPage = $(this).text();
+            let currentPage = $(this).text();
             $(".pagination li").removeClass("active");
             $(this).addClass("active");
-            switchPages(limitPerPage, totalPages, currentPage, "other")
+            switchPages(limitPerPage, currentPage, "other")
         }
     });
 }
 
 // This method hands off the current page to the showHideButtons function when a new button is either clicked or inputted
-function switchPages(limitPerPage, totalPages, currentPage, type) {
-
+function switchPages(limitPerPage, currentPage, type) {
+    let totalPages = maxPages;
     $("#loop .items-to-paginate").hide();
-    var grandTotal = limitPerPage * currentPage;
-
+    let grandTotal = limitPerPage * currentPage;
     // If the next or previous button was pressed, this is how we make sure the "..." li elements aren't holding real data
     if (type === "next-page" || type === "previous-page") {
         if (currentPage < 2) {
-            for (var i = grandTotal - limitPerPage; i < grandTotal; i++) {
+            for (let i = grandTotal - limitPerPage; i < grandTotal; i++) {
                 $("#loop .items-to-paginate:eq(" + i + ")").show();
             }
         } else if (currentPage > 2 && currentPage < 6) {
-            for (var i = grandTotal - limitPerPage - 10; i < grandTotal - 10; i++) {
+            for (let i = grandTotal - limitPerPage - 10; i < grandTotal - 10; i++) {
                 $("#loop .items-to-paginate:eq(" + i + ")").show();
             }
-        } else if (currentPage > 6 && currentPage < 15) {
-            for (var i = grandTotal - limitPerPage - 20; i < grandTotal - 20; i++) {
+        } else if (currentPage > 6 && currentPage < totalPages - 3) {
+            for (let i = grandTotal - limitPerPage - 20; i < grandTotal - 20; i++) {
                 $("#loop .items-to-paginate:eq(" + i + ")").show();
             }
         } else {
-            for (var i = grandTotal - limitPerPage - 30; i < grandTotal - 30; i++) {
+            for (let i = grandTotal - limitPerPage - 30; i < grandTotal - 30; i++) {
                 $("#loop .items-to-paginate:eq(" + i + ")").show();
             }
         }
     } else {
-        for (var i = grandTotal - limitPerPage; i < grandTotal ; i ++) {
+        for (let i = grandTotal - limitPerPage; i < grandTotal ; i ++) {
             $("#loop .items-to-paginate:eq(" + i + ")").show();
         }
     }
@@ -265,16 +277,17 @@ function switchPages(limitPerPage, totalPages, currentPage, type) {
         }
     }
     if (totalPages > paginationRange) {
-        showHideButtons(currentPage, totalPages);
+        showHideButtons(currentPage);
     }
 }
 
 // This method makes the correct buttons show when you select a new button
-function showHideButtons(currentPage, totalPages) {
-    var x = (document.getElementsByClassName("temp-button")[currentPage - 1]).id;
+function showHideButtons(currentPage) {
+    let totalPages = maxPages;
+    let x = (document.getElementsByClassName("temp-button")[currentPage - 1]).id;
 
     if ((x.split('-')[1]) == 1 || currentPage < 5) {
-        for (var i = 1; i < totalPages; i++) {
+        for (let i = 1; i < totalPages; i++) {
             display([i.toString()],[]);
             if ((i > 0) && (i < 5)) {
                 display([], [i.toString()]);
@@ -282,7 +295,7 @@ function showHideButtons(currentPage, totalPages) {
         }
         display(["dot-1", "dot-3"], ["dot-2"]);
     } else if ((x.split('-')[1]) == 4 || currentPage > totalPages - 1) {
-        for (var i = 2; i < totalPages; i++) {
+        for (let i = 2; i < totalPages; i++) {
             display([i.toString()],[]);
             if ((i > totalPages - 4)) {
                 display([], [i.toString()]);
@@ -290,7 +303,7 @@ function showHideButtons(currentPage, totalPages) {
         }
         display(["dot-1", "dot-3"], ["dot-2"]);
     } else {
-        for (var i = 2; i < totalPages; i++) {
+        for (let i = 2; i < totalPages; i++) {
             display([i.toString()],[]);
             if ((i > (parseInt(x) - 2)) && (i < (parseInt(x) + 2))) {
                 display([], [i.toString()]);
@@ -304,19 +317,20 @@ function showHideButtons(currentPage, totalPages) {
 // iterates through the lists, and sets them to their desired display type
 function display(none, inline) {
     if (none.length !== 0) {
-        for (var i = 0; i < none.length; i++) {
+        for (let i = 0; i < none.length; i++) {
             document.getElementById(none[i].toString()).style.display = "none";
         }
     }
     if (inline.length !== 0) {
-        for (var i = 0; i < inline.length; i ++) {
+        for (let i = 0; i < inline.length; i ++) {
             document.getElementById(inline[i].toString()).style.display = "inline";
         }
     }
 }
 
 // This method creates all the initial buttons for pagination, along with the dots
-function createButtons(totalPages, limitPerPage) {
+function createButtons(limitPerPage) {
+    let totalPages = maxPages;
 
     $(" #loop .items-to-paginate:gt(" + (limitPerPage - 1) + ")").hide();
 
@@ -325,7 +339,7 @@ function createButtons(totalPages, limitPerPage) {
     if (totalPages > paginationRange + 1) {
         $(".pagination").append("<li id='dot-1' style='display:none;' class='dot current-page temp-button'><a href='javascript:void(0)'>...</a></li>");
 
-        for (var i = 2; i <= totalPages; i++) {
+        for (let i = 2; i <= totalPages; i++) {
             if (i > 4 && i < (totalPages - 3)) {
                 if (i > 4 && i < 6 && totalPages > 6) {
                     $(".pagination").append("<li id='dot-2' class='dot current-page temp-button'><a href='javascript:void(0)'>...</a></li>");
@@ -341,7 +355,7 @@ function createButtons(totalPages, limitPerPage) {
         $(".pagination").append("<li id='" + totalPages + "' class='current-page temp-button'><a href='javascript:void(0)'>" + totalPages + "</a></li>");
 
     } else {
-        for (var i = 2; i <= totalPages; i++) {
+        for (let i = 2; i <= totalPages; i++) {
              $(".pagination").append("<li id='" + i + "'  class='current-page temp-button'><a href='javascript:void(0)'>" + i + "</a></li>");
         }
     }
