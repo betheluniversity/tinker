cceb6681f7a1d43f7feccf19c58dfdb49a3a0810
Philip Gibbens <phg49389@bethel.edu>
2019-05-16 15:28:32 -0500

    Updated relevant unit test

--- testing_suite/unit_tests/faculty_bios_tests/test_get_correct_workflow_id.py
+++ testing_suite/unit_tests/faculty_bios_tests/test_get_correct_workflow_id.py
@@ -15,35 +15,37 @@ class GetCorrectWorkflowIDTestCase(FacultyBiosControllerBaseTestCase):
     #######################
 
     def test_get_correct_workflow(self):
+        # Set groups to an empty string so that it's a normal check
+        groups = ''
         test_add_data = {
             'schools': 'College of Arts and Sciences'
         }
-        response = self.controller.get_correct_workflow_id(test_add_data)
+        response = self.controller.get_correct_workflow_id(test_add_data, groups)
         self.assertTrue(isinstance(response, str))
         self.assertEqual(response, app.config['FACULTY_BIOS_WORKFLOW_CAS_ID'])
 
         test_add_data = {
             'schools': 'Graduate School'
         }
-        response = self.controller.get_correct_workflow_id(test_add_data)
+        response = self.controller.get_correct_workflow_id(test_add_data, groups)
         self.assertTrue(isinstance(response, str))
         self.assertEqual(response, app.config['FACULTY_BIOS_WORKFLOW_CAPSGS_ID'])
 
         test_add_data = {
             'schools': 'College of Adult and Professional Studies'
         }
-        response = self.controller.get_correct_workflow_id(test_add_data)
+        response = self.controller.get_correct_workflow_id(test_add_data, groups)
         self.assertTrue(isinstance(response, str))
         self.assertEqual(response, app.config['FACULTY_BIOS_WORKFLOW_CAPSGS_ID'])
 
         test_add_data = {
             'schools': 'Bethel Seminary'
         }
-        response = self.controller.get_correct_workflow_id(test_add_data)
+        response = self.controller.get_correct_workflow_id(test_add_data, groups)
         self.assertTrue(isinstance(response, str))
         self.assertEqual(response, app.config['FACULTY_BIOS_WORKFLOW_SEM_ID'])
 
         test_add_data = {}
-        response = self.controller.get_correct_workflow_id(test_add_data)
+        response = self.controller.get_correct_workflow_id(test_add_data, groups)
         self.assertTrue(isinstance(response, str))
         self.assertEqual(response, app.config['FACULTY_BIOS_WORKFLOW_CAS_ID'])
