84156b629debea8494687fd544c33cb5ac635573
Jordan Jasik <joj28267@bethel.edu>
2019-06-17 14:32:33 -0500

    Handle on paste too

--- tinker/static/ckeditor/plugins/bu_removecomments/plugin.js
+++ tinker/static/ckeditor/plugins/bu_removecomments/plugin.js
@@ -1,7 +1,17 @@
 CKEDITOR.plugins.add('bu_removecomments', {
     init: function (editor) {
+        // This runs when the editor is initialized. This is needed for editing existing faculty bios
         var text = editor.getData();
         text = text.replace(/<!--(.*?)-->/gs, "");
         editor.setData(text);
+
+        // This checks content on paste, needed for editing or creating new faculty bios
+        editor.on('paste', function(e){
+            setTimeout(function(){ // Need a timeout so the paste is registered before we change any data.
+                var text = editor.getData();
+                text = text.replace(/<!--(.*?)-->/gs, "");
+                editor.setData(text);
+            }, 100);
+        });
     }
 });
