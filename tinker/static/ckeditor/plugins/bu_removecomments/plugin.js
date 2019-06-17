ccc95a57f45e943a4c9c64d43783421a590a3549
Jordan Jasik <joj28267@bethel.edu>
2019-06-17 14:34:10 -0500

    Unneeded parameter

--- tinker/static/ckeditor/plugins/bu_removecomments/plugin.js
+++ tinker/static/ckeditor/plugins/bu_removecomments/plugin.js
@@ -6,7 +6,7 @@ CKEDITOR.plugins.add('bu_removecomments', {
         editor.setData(text);
 
         // This checks content on paste, needed for editing or creating new faculty bios
-        editor.on('paste', function(e){
+        editor.on('paste', function(){
             setTimeout(function(){ // Need a timeout so the paste is registered before we change any data.
                 var text = editor.getData();
                 text = text.replace(/<!--(.*?)-->/gs, "");
