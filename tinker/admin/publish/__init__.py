ba66ad1b7a96cd40e64239ad1a8a1b45fc3b3a05
Caleb Schwarze <c-schwarze@bethel.edu>
2019-07-19 15:07:50 -0500

    rework the publish program feeds

--- tinker/admin/publish/__init__.py
+++ tinker/admin/publish/__init__.py
@@ -10,7 +10,7 @@ from flask_classy import FlaskView, route
 
 # Local
 from tinker.admin.publish.publish_manager_controller import PublishManagerController
-from tinker.tinker_controller import admin_permissions
+from tinker.tinker_controller import admin_permissions, requires_auth
 
 
 class PublishView(FlaskView):
@@ -21,7 +21,8 @@ class PublishView(FlaskView):
 
     # This method is called before any request to check user's credentials
     def before_request(self, name, **kwargs):
-        admin_permissions(self)
+        if '/program-feeds/public/' not in request.url:
+            admin_permissions(self)
 
     # Publish manager's homepage
     def index(self):
@@ -33,41 +34,12 @@ class PublishView(FlaskView):
         return render_template('admin/publish/program-feeds.html', **locals())
 
     # in puppet, we don't require CAS auth for this, so we can call it from cron and load it normally
+    # @requires_auth
     @route("/program-feeds/public/<destination>")
+    def cron_publish_program_feeds_return(self, destination=''):
+        return self.base.publish_program_feeds(destination)
+
     @route("/program-feeds/<destination>")
     def publish_program_feeds_return(self, destination=''):
-        if destination != "production":
-            destination = "staging"
+        return self.base.publish_program_feeds(destination)
 
-        # get results
-        results = self.base.search_data_definitions("*program-feed*")
-        if results.matches is None or results.matches == "":
-            results = []
-        else:
-            results = results.matches.match
-
-        final_results = []
-
-        # publish all results' relationships
-        for result in results:
-            type = result.type
-            id = result.id
-
-            if type == "block" and '/base-assets/' not in result.path.path and '_testing/' not in result.path.path:
-                try:
-                    relationships = self.base.list_relationships(id, type)
-                    pages = relationships.subscribers.assetIdentifier
-                    pages_added = []
-                    for page in pages:
-                        resp = self.base.publish(page.id, "page", destination)
-                        if 'success = "false"' in str(resp):
-                            message = resp['message']
-                        else:
-                            message = 'Published'
-                        pages_added.append({'id': page.id, 'path': page.path.path, 'message': message})
-                except:
-                    continue
-
-                final_results.append({'id': result.id, 'path': result.path.path, 'pages': pages_added})
-
-        return render_template('admin/publish/program-feeds-table.html', **locals())
--- tinker/admin/publish/publish_manager_controller.py
+++ tinker/admin/publish/publish_manager_controller.py
@@ -1,6 +1,9 @@
 # Global
 from datetime import datetime
 
+# Packages
+from flask import render_template
+
 # Local
 from tinker.tinker_controller import TinkerController
 
@@ -19,3 +22,40 @@ class PublishManagerController(TinkerController):
         }
         response = self.search_cascade(search_information)
         return response
+
+    def publish_program_feeds(self, destination):
+        if destination != "production":
+            destination = "staging"
+
+        # get results
+        results = self.search_data_definitions("*program-feed*")
+        if results.matches is None or results.matches == "":
+            results = []
+        else:
+            results = results.matches.match
+
+        final_results = []
+
+        # publish all results' relationships
+        for result in results:
+            type = result.type
+            id = result.id
+
+            if type == "block" and '/base-assets/' not in result.path.path and '_testing/' not in result.path.path:
+                try:
+                    relationships = self.list_relationships(id, type)
+                    pages = relationships.subscribers.assetIdentifier
+                    pages_added = []
+                    for page in pages:
+                        resp = self.publish(page.id, "page", destination)
+                        if 'success = "false"' in str(resp):
+                            message = resp['message']
+                        else:
+                            message = 'Published'
+                        pages_added.append({'id': page.id, 'path': page.path.path, 'message': message})
+                except:
+                    continue
+
+                final_results.append({'id': result.id, 'path': result.path.path, 'pages': pages_added})
+
+        return render_template('admin/publish/program-feeds-table.html', **locals())
